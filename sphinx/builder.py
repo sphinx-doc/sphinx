31e22c0351a547d86ab394e26aedc335d9c6edf6
Georg Brandl <georg@python.org>
2008-03-14 15:41:33 +0000

    Another typo.

--- sphinx/builder.py
+++ sphinx/builder.py
@@ -979,7 +979,7 @@ class CheckExternalLinksBuilder(Builder):
         except HTTPError, err:
             if err.code == 403 and uri.startswith('http://en.wikipedia.org/'):
                 # Wikipedia blocks requests from urllib User-Agent
-                return 0
+                return (0, 0)
             return (2, str(err))
         except Exception, err:
             return (2, str(err))
