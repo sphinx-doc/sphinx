e8a6143f8b9b8fd5394fda3deaa7f6d10d2afb40
Georg Brandl <georg@python.org>
2008-03-16 11:44:35 +0000

    sphinx.web is currently broken.

--- sphinx-web.py
+++ sphinx-web.py
@@ -10,5 +10,8 @@
 import sys
 
 if __name__ == '__main__':
+    print 'sphinx.web currently doesn\'t work -- it will undergo a ' \
+          'serious rewrite soon.'
+    sys.exit()
     from sphinx.web import main
     sys.exit(main(sys.argv))
