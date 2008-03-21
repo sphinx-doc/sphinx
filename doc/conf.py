5f78bc436b33d9f233cfdd61066f5a72052fe0ef
Georg Brandl <georg@python.org>
2008-03-21 15:20:50 +0000

    Use Windows-friendlier dirnames.

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ sys.path.append(os.path.dirname(__file__))
 extensions = ['ext', 'sphinx.ext.autodoc', 'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -77,7 +77,7 @@ html_style = 'sphinxdoc.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
