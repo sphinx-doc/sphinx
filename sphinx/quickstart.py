ab34a7dd71cf7b4b451fa40eb3c2ca0d9d7b7222
Georg Brandl <georg@python.org>
2008-03-22 19:52:21 +0000

    Fix module name.

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -38,7 +38,7 @@ import sys
 # ---------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.addons.*') or your custom ones.
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 #extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
