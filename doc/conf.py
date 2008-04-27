0ee5e392538c9ce253fc7f0bb6de57af666da071
Georg Brandl <georg@python.org>
2008-04-27 20:12:41 +0000

    Fetch Sphinx version from Sphinx itself.

--- doc/conf.py
+++ doc/conf.py
@@ -40,9 +40,10 @@ copyright = '2008, Georg Brandl'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.1'
+import sphinx
+version = sphinx.__version__
 # The full version, including alpha/beta/rc tags.
-release = '0.1'
+release = version
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
