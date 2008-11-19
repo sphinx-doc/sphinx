4f8a18d84181a0cf14ddf8fa84d56e70e2093a2c
Georg Brandl <georg@python.org>
2008-11-19 18:31:11 +0100

    Use an explicit language.

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,8 @@ import sys, os, re
 # General configuration
 # ---------------------
 
+language = 'en'
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.addons.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo']
