18d1581eece3c54db47954ab460c98220835e121
Georg Brandl <georg@python.org>
2008-11-29 20:03:56 +0100

    This is version 0.6.

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -13,8 +13,8 @@ import sys
 from os import path
 
 __revision__ = '$Revision$'
-__version__ = '0.5'
-__released__ = '0.5'
+__version__ = '0.6'
+__released__ = '0.6 (hg)'
 
 package_dir = path.abspath(path.dirname(__file__))
 
