6ee8b51c01b9cabd34d2ed3112047e0a08f10341
Georg Brandl <georg@python.org>
2009-02-18 00:21:11 +0100

    Dont prefer SVG before PNG until SVG is supported and its handling in Sphinx sorted out.

--- sphinx/builders/html.py
+++ sphinx/builders/html.py
@@ -55,8 +55,8 @@ class StandaloneHTMLBuilder(Builder):
     out_suffix = '.html'
     link_suffix = '.html'  # defaults to matching out_suffix
     indexer_format = js_index
-    supported_image_types = ['image/svg+xml', 'image/png', 'image/gif',
-                             'image/jpeg']
+    supported_image_types = ['image/png', 'image/gif', 'image/jpeg',
+                             'image/svg+xml']
     searchindex_filename = 'searchindex.js'
     add_permalinks = True
     embedded = False  # for things like HTML help or Qt help: suppresses sidebar
