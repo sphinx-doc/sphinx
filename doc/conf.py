d3a7f004f07578817af9e9290d01f707da00652f
Georg Brandl <georg@python.org>
2009-02-20 11:09:54 +0100

    Oops.

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ release = version
 show_authors = True
 
 # The HTML template theme.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # A list of ignored prefixes names for module index sorting.
 modindex_common_prefix = ['sphinx.']
