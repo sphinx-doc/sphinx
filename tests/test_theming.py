9199b21f85aed69a6a28ed0a622c0d145a397cab
Georg Brandl <georg@python.org>
2009-07-05 12:22:01 +0200

    Add new builtin theme to test suite.

--- tests/test_theming.py
+++ tests/test_theming.py
@@ -24,7 +24,7 @@ def test_theme_api(app):
 
     # test Theme class API
     assert set(Theme.themes.keys()) == \
-           set(['basic', 'default', 'sphinxdoc', 'traditional',
+           set(['basic', 'default', 'scrolls', 'sphinxdoc', 'traditional',
                 'testtheme', 'ziptheme'])
     assert Theme.themes['testtheme'][1] is None
     assert isinstance(Theme.themes['ziptheme'][1], zipfile.ZipFile)
