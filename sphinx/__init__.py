0899e017a9a0963408c6cff8c3c19a20d3efbcaa
Georg Brandl <georg@python.org>
2009-09-04 00:14:18 +0200

    Post-0.6.3 changes.

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -12,7 +12,7 @@
 import sys
 from os import path
 
-__version__ = '0.6.3'
+__version__ = '0.6.3+'
 __released__ = '0.6.3'  # used when Sphinx builds its own docs
 
 package_dir = path.abspath(path.dirname(__file__))
