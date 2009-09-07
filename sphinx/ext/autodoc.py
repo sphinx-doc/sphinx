6769daa80a8ce873072a88a751b4ecb05f66467b
Georg Brandl <georg@python.org>
2009-09-07 23:18:17 +0200

    autodoc: generate directives including domain name.

--- sphinx/ext/autodoc.py
+++ sphinx/ext/autodoc.py
@@ -390,9 +390,11 @@ class Documenter(object):
 
     def add_directive_header(self, sig):
         """Add the directive header and options to the generated content."""
+        domain = getattr(self, 'domain', 'py')
         directive = getattr(self, 'directivetype', self.objtype)
         name = self.format_name()
-        self.add_line(u'.. %s:: %s%s' % (directive, name, sig), '<autodoc>')
+        self.add_line(u'.. %s:%s:: %s%s' % (domain, directive, name, sig),
+                      '<autodoc>')
         if self.options.noindex:
             self.add_line(u'   :noindex:', '<autodoc>')
         if self.objpath:
