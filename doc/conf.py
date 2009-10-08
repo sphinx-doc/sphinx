a51e3a4129d0f24a93d89d2bc053fd0192450257
Georg Brandl <georg@python.org>
2009-10-08 09:14:01 +0200

    Add missing extension.

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,8 @@ import sys, os, re
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.addons.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+              'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
