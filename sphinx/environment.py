523b16f005396a3f995a2c4daaae942130eb4461
Georg Brandl <georg@python.org>
2010-02-28 09:49:23 +0100

    Revert revision 0530a8aaaa9a; it does not work if dependent files are involved.

--- sphinx/environment.py
+++ sphinx/environment.py
@@ -616,8 +616,8 @@ class BuildEnvironment:
         self.note_citations_from(docname, doctree)
         self.build_toc_from(docname, doctree)
 
-        # store file modification time, for outdated files detection
-        self.all_docs[docname] = path.getmtime(src_path)
+        # store time of build, for outdated files detection
+        self.all_docs[docname] = time.time()
 
         if app:
             app.emit('doctree-read', doctree)
