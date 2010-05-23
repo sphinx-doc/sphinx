9116cf9779bc84a4111d00b85b83a66c67351e51
Georg Brandl <georg@python.org>
2010-05-23 12:05:07 +0200

    Mention "make latexpdf".

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -405,8 +405,8 @@ latex:
 \t$(SPHINXBUILD) -b latex $(ALLSPHINXOPTS) $(BUILDDIR)/latex
 \t@echo
 \t@echo "Build finished; the LaTeX files are in $(BUILDDIR)/latex."
-\t@echo "Run \\`make all-pdf' or \\`make all-ps' in that directory to" \\
-\t      "run these through (pdf)latex."
+\t@echo "Run \\`make' in that directory to run these through (pdf)latex" \\
+\t      "(use \\`make latexpdf' here to do that automatically)."
 
 latexpdf: latex
 \t$(SPHINXBUILD) -b latex $(ALLSPHINXOPTS) $(BUILDDIR)/latex
