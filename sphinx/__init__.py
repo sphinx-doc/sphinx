4a796be021bc0e93c5fb40b12251b2bf079fa0b0
Georg Brandl <georg@python.org>
2010-05-25 00:53:40 +0200

    Prepare for 0.6.6 release.

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -12,8 +12,8 @@
 import sys
 from os import path
 
-__version__ = '0.6.5+'
-__released__ = '0.6.5'  # used when Sphinx builds its own docs
+__version__ = '0.6.6'
+__released__ = '0.6.6'  # used when Sphinx builds its own docs
 
 package_dir = path.abspath(path.dirname(__file__))
 
