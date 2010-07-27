3d6dd4d1e7a56921943c191ed87316bd5aded7e9
Georg Brandl <georg@python.org>
2010-07-27 19:52:25 +0200

    Post-release updates.

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -12,7 +12,7 @@
 import sys
 from os import path
 
-__version__ = '1.0.1'
+__version__ = '1.0.1+'
 __released__ = '1.0.1'  # used when Sphinx builds its own docs
 
 package_dir = path.abspath(path.dirname(__file__))
