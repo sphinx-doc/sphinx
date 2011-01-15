0806a96a49a5121690e665f68a09759e19c547d7
Georg Brandl <georg@python.org>
2011-01-15 17:21:18 +0100

    Post-release bumps.

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -12,7 +12,7 @@
 import sys
 from os import path
 
-__version__  = '1.0.7'
+__version__  = '1.0.7+'
 __released__ = '1.0.7'  # used when Sphinx builds its own docs
 
 package_dir = path.abspath(path.dirname(__file__))
