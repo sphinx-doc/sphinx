569516dd0ee22b319d2019d359f8f723914506c4
Georg Brandl <georg@python.org>
2011-05-15 11:37:00 +0200

    Closes #693: Fix double encoding done when writing HTMLHelp .hhk files.

--- sphinx/builders/htmlhelp.py
+++ sphinx/builders/htmlhelp.py
@@ -258,7 +258,7 @@ class HTMLHelpBuilder(StandaloneHTMLBuilder):
             def write_index(title, refs, subitems):
                 def write_param(name, value):
                     item = '    <param name="%s" value="%s">\n' % (name, value)
-                    f.write(item.encode(self.encoding, 'xmlcharrefreplace'))
+                    f.write(item)
                 title = cgi.escape(title)
                 f.write('<LI> <OBJECT type="text/sitemap">\n')
                 write_param('Keyword', title)
