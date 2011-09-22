c8b57044cde77c3dcc3d220023a1e1d4cbad1cb0
Georg Brandl <georg@python.org>
2011-09-22 15:32:02 +0200

    Fix long line.

--- sphinx/ext/coverage.py
+++ sphinx/ext/coverage.py
@@ -213,7 +213,8 @@ class CoverageBuilder(Builder):
                         op.write('\n')
                     if undoc['classes']:
                         op.write('Classes:\n')
-                        for name, methods in sorted(undoc['classes'].iteritems()):
+                        for name, methods in sorted(
+                                                 undoc['classes'].iteritems()):
                             if not methods:
                                 op.write(' * %s\n' % name)
                             else:
