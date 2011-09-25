142c15093a3cd5cea21450d3b9799140434ae30e
Georg Brandl <georg@python.org>
2011-09-25 08:31:27 +0200

    Upgrade the version requirements.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ A development egg can be found `here
 <http://bitbucket.org/birkenfeld/sphinx/get/tip.gz#egg=Sphinx-dev>`_.
 '''
 
-requires = ['Pygments>=0.8', 'Jinja2>=2.2', 'docutils>=0.5']
+requires = ['Pygments>=1.2', 'Jinja2>=2.3', 'docutils>=0.7']
 
 if sys.version_info < (2, 4):
     print('ERROR: Sphinx requires at least Python 2.4 to run.')
