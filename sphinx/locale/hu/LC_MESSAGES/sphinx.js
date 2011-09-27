6c14e874b16061e8666e507f4f2824c70455a61b
Tibor Toth <szunyog@gmail.com>
2011-09-27 07:19:56 +0200

    Added Hungarian translation.

---
+++ sphinx/locale/hu/LC_MESSAGES/sphinx.js
@@ -0,0 +1,1 @@
+Documentation.addTranslations({"locale": "hu", "plural_expr": "(n != 1)", "messages": {"Hide Search Matches": "Keresési Találatok Elrejtése", "Permalink to this definition": "Hivatkozás erre a definícióra", "Expand sidebar": "Oldalsáv kinyitása", "Permalink to this headline": "Hivatkozás erre a fejezetcímre", "Collapse sidebar": "Oldalsáv összezárása"}});
