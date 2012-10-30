c411b51fbbdaae201331123c5a449ea7f5f8b0c8
Georg Brandl <georg@python.org>
2012-10-30 10:48:48 +0100

    Closes #998: fix manpage writer in expectation of docutils 0.10 API change

--- sphinx/writers/manpage.py
+++ sphinx/writers/manpage.py
@@ -72,6 +72,11 @@ class ManualPageTranslator(BaseTranslator):
         # since self.append_header() is never called, need to do this here
         self.body.append(MACRO_DEF)
 
+        # Overwrite admonition label translations with our own
+        for label, translation in admonitionlabels.items():
+            self.language.labels[label] = self.deunicode(translation)
+
+
     # overwritten -- added quotes around all .TH arguments
     def header(self):
         tmpl = (".TH \"%(title_upper)s\" \"%(manual_section)s\""
@@ -192,12 +197,6 @@ class ManualPageTranslator(BaseTranslator):
         self.visit_admonition(node)
     def depart_seealso(self, node):
         self.depart_admonition(node)
-
-    # overwritten -- use our own label translations
-    def visit_admonition(self, node, name=None):
-        if name:
-            self.body.append('.IP %s\n' %
-                             self.deunicode(admonitionlabels.get(name, name)))
 
     def visit_productionlist(self, node):
         self.ensure_eol()
