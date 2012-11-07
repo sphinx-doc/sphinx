378b18d85ea0f9f7bf983a532876fd2b9b4bbdf4
Takayuki Shimizukawa <shimizukawa@gmail.com>
2012-11-07 19:22:01 +0900

    #1024 make.bat improve error message if Sphinx is not found

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -584,15 +584,15 @@ if "%%1" == "clean" (
 
 %%SPHINXBUILD%% 2> nul
 if errorlevel 9009 (
-    echo.
-    echo.The 'sphinx-build' command was not found. Make sure Sphinx is
-    echo.installed, then set the SPHINXBUILD environment variable to point
-    echo.to the full path of the 'sphinx-build' executable. Alternatively
-    echo.you may add the Sphinx directory to PATH.
-    echo.
-    echo.If you don't have Sphinx installed, grab it from
-    echo.http://sphinx-doc.org/
-    exit /b 1
+\techo.
+\techo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+\techo.installed, then set the SPHINXBUILD environment variable to point
+\techo.to the full path of the 'sphinx-build' executable. Alternatively you
+\techo.may add the Sphinx directory to PATH.
+\techo.
+\techo.If you don't have Sphinx installed, grab it from
+\techo.http://sphinx-doc.org/
+\texit /b 1
 )
 
 if "%%1" == "html" (
