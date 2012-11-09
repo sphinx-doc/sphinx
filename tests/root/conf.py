34cbd684a91d6314afe3a5828cfd488ca0d7711b
Jonathan Waltman <jonathan.waltman@gmail.com>
2012-11-08 21:16:41 -0600

    Update sys.path to avoid autodoc warnings when building docs in tests/root.

--- tests/root/conf.py
+++ tests/root/conf.py
@@ -3,6 +3,7 @@
 import sys, os
 
 sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('..'))
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.jsmath', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.autosummary',
