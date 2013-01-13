95c45a0ab3d66d6e4b8140203eb1e897ef1f727a
Georg Brandl <georg@python.org>
2013-01-13 14:05:40 +0100

    autodoc: debug module analyzer failure

--- sphinx/ext/autodoc.py
+++ sphinx/ext/autodoc.py
@@ -717,7 +717,8 @@ class Documenter(object):
             # parse right now, to get PycodeErrors on parsing (results will
             # be cached anyway)
             self.analyzer.find_attr_docs()
-        except PycodeError:
+        except PycodeError, err:
+            self.env.app.debug('[autodoc] module analyzer failed: %s', err)
             # no source file -- e.g. for builtin and C modules
             self.analyzer = None
             # at least add the module.__file__ as a dependency
