d96de9d12880d8a10e0da301366a414121d88798
Takayuki Shimizukawa <shimizukawa@gmail.com>
2013-11-17 03:39:27 +0000

    Fix test_linkcode.test_html fails with C locale and Python 3. Closes #1311

--- tests/test_linkcode.py
+++ tests/test_linkcode.py
@@ -12,15 +12,12 @@
 import os
 from util import with_app
 
+
 @with_app(srcdir='(temp)', buildername='html', tags=['test_linkcode'])
 def test_html(app):
     app.builder.build_all()
 
-    fp = open(os.path.join(app.outdir, 'objects.html'), 'r')
-    try:
-        stuff = fp.read()
-    finally:
-        fp.close()
+    stuff = (app.outdir / 'objects.html').text(encoding='utf-8')
 
     assert 'http://foobar/source/foolib.py' in stuff
     assert 'http://foobar/js/' in stuff
