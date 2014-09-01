c884bfacaba2c658eb6c96bbbd43a08a3700564f
Takayuki Shimizukawa <shimizukawa@gmail.com>
2014-09-02 00:16:50 +0900

    Post-release bump.

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -15,7 +15,7 @@
 import sys
 from os import path
 
-__version__  = '1.2.3'
+__version__  = '1.2.3+'
 __released__ = '1.2.3'  # used when Sphinx builds its own docs
 # version info for better programmatic use
 # possible values for 3rd element: 'alpha', 'beta', 'rc', 'final'
