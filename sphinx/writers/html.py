cd4437122b3ed1a27c88c2b3c0c1dbcd86a8409f
Georg Brandl <georg@python.org>
2014-10-06 15:34:42 +0200

    Include placeholder in numfig prefix i18n strings so that they do not end in spaces

--- sphinx/writers/html.py
+++ sphinx/writers/html.py
@@ -251,7 +251,7 @@ class HTMLTranslator(BaseTranslator):
                 self.body.append(self.starttag(node, 'span', '', CLASS='caption-number'))
                 prefix = self.builder.config.numfig_prefix.get(figtype, '')
                 numbers = self.builder.fignumbers[figtype][figure_id]
-                self.body.append(prefix + '.'.join(map(str, numbers)) + " ")
+                self.body.append(prefix % '.'.join(map(str, numbers)) + ' ')
                 self.body.append('</span>')
 
         if isinstance(node.parent, nodes.figure):
