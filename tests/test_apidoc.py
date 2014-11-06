e6fa449e85da997492b397104d02379b0f7e7b36
Georg Brandl <georg@python.org>
2014-11-06 09:11:20 +0100

    Add a basic test for sphinx.apidoc.

---
+++ tests/test_apidoc.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+    test_apidoc
+    ~~~~~~~~~~~
+
+    Test the sphinx.apidoc module.
+
+    :copyright: Copyright 2007-2014 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from __future__ import print_function
+
+import sys
+
+from sphinx import apidoc
+
+from util import with_tempdir, with_app, rootdir
+
+
+@with_tempdir
+def test_simple(tempdir):
+    codedir = rootdir / 'root'
+    outdir = tempdir / 'out'
+    args = ['sphinx-apidoc', '-o', outdir, '-F', codedir]
+    apidoc.main(args)
+
+    assert (outdir / 'conf.py').isfile()
+    assert (outdir / 'autodoc_fodder.rst').isfile()
+    assert (outdir / 'index.rst').isfile()
+
+    @with_app('text', srcdir=outdir)
+    def assert_build(app, status, warning):
+        app.build()
+        print(status.getvalue())
+        print(warning.getvalue())
+
+    sys.path.append(codedir)
+    try:
+        assert_build()
+    finally:
+        sys.path.remove(codedir)
