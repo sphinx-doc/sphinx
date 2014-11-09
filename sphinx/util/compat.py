09d2c399d3b3e725aa45d375ce24ca4198cca9f9
Georg Brandl <georg@python.org>
2014-11-09 19:04:01 +0100

    Deprecate make_admonition.

--- sphinx/util/compat.py
+++ sphinx/util/compat.py
@@ -8,19 +8,19 @@
     :copyright: Copyright 2007-2014 by the Sphinx team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
+import warnings
 
 from docutils import nodes
 
 from docutils import __version__ as _du_version
 docutils_version = tuple(int(x) for x in _du_version.split('.')[:2])
 
+
 def make_admonition(node_class, name, arguments, options, content, lineno,
                     content_offset, block_text, state, state_machine):
-    #if not content:
-    #    error = state_machine.reporter.error(
-    #        'The "%s" admonition is empty; content required.' % (name),
-    #        nodes.literal_block(block_text, block_text), line=lineno)
-    #    return [error]
+    warnings.warn('make_admonition is deprecated, use '
+                  'docutils.parsers.rst.directives.admonitions.BaseAdmonition '
+                  'instead', DeprecationWarning, stacklevel=1)
     text = '\n'.join(content)
     admonition_node = node_class(text)
     if arguments:
