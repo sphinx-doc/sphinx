6c1ba34614ee5f2fae946babceefc6fb9ae34e10
shimizukawa <shimizukawa@gmail.com>
2015-04-05 18:22:15 +0900

    Fixes #1834: compatibility for docutils-0.13: handle_io_errors keyword argument for docutils.io.FileInput cause TypeError. The argument is already deprecated and no effects from docutils-0.10.

--- sphinx/environment.py
+++ sphinx/environment.py
@@ -141,8 +141,6 @@ class SphinxFileInput(FileInput):
     def __init__(self, app, env, *args, **kwds):
         self.app = app
         self.env = env
-        # don't call sys.exit() on IOErrors
-        kwds['handle_io_errors'] = False
         kwds['error_handler'] = 'sphinx'  # py3: handle error on open.
         FileInput.__init__(self, *args, **kwds)
 
