9c53796f87e5957744bcc8b990ef8387446b9964
anatoly techtonik <techtonik@gmail.com>
2015-10-10 16:40:59 +0300

    add inverse of util.os_path for canonical path generation

--- sphinx/util/osutil.py
+++ sphinx/util/osutil.py
@@ -38,6 +38,10 @@ SEP = "/"
 
 def os_path(canonicalpath):
     return canonicalpath.replace(SEP, path.sep)
+    
+def canon_path(nativepath):
+    """Return path in OS-independent form"""
+    return nativepath.replace(path.sep, SEP)
 
 
 def relative_uri(base, to):
