4c7db6c67d26151c90aa5f1949f007776edd28f9
shimizukawa <shimizukawa@gmail.com>
2015-11-29 22:23:56 +0900

    fix line length

--- tests/test_util_nodes.py
+++ tests/test_util_nodes.py
@@ -136,8 +136,8 @@ def test_extract_messages_without_rawsource():
     """
     Check node.rawsource is fall-backed by using node.astext() value.
 
-    `extract_message` which is used from Sphinx i18n feature drop ``not node.rawsource`` nodes.
-    So, all nodes which want to translate must have ``rawsource`` value.
+    `extract_message` which is used from Sphinx i18n feature drop ``not node.rawsource``
+    nodes. So, all nodes which want to translate must have ``rawsource`` value.
     However, sometimes node.rawsource is not set.
 
     For example: recommonmark-0.2.0 doesn't set rawsource to `paragraph` node.
