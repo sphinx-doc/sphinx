f81ab1e8b9690902721901eda378a47d7dc6e8a0
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-01-01 12:06:46 +0900

    Fix #2162: Add Sphinx.add_source_parser() to add source_suffix and source_parsers from extension

---
+++ tests/roots/test-add_source_parser/conf.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from docutils.parsers import Parser
+
+sys.path.insert(0, os.path.abspath('.'))
+
+
+class DummyMarkdownParser(Parser):
+    pass
+
+
+extensions = ['test_source_parser']
+source_suffix = ['.rst', '.md']
+source_parsers = {
+    '.md': DummyMarkdownParser
+}
