f44fd9f9949f1d324dd32ef4d757db0fc20d5d03
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-01-17 19:38:05 +0900

    Fix broken test by 14f58e66199393a2fb4903dc807c82ca56951340

--- tests/roots/test-ext-graphviz/conf.py
+++ tests/roots/test-ext-graphviz/conf.py
@@ -2,12 +2,9 @@
 
 extensions = ['sphinx.ext.graphviz']
 master_doc = 'index'
-<<<<<<< 492f60c37185dccee1e42c01fb5d87ed42f45608
 exclude_patterns = ['_build']
-=======
 
 latex_documents = [
     (master_doc, 'SphinxTests.tex', 'Sphinx Tests Documentation',
      'Georg Brandl', 'manual'),
 ]
->>>>>>> ``sphinx.ext.graphviz``: show graph image in inline by default
