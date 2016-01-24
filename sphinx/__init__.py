7d946358d3bdacf6de71294d82c362e8d1a9eb28
shimizukawa <shimizukawa@gmail.com>
2016-01-24 13:10:31 +0900

    Bump version

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -15,13 +15,13 @@
 import sys
 from os import path
 
-__version__  = '1.3.5'
-__released__ = '1.3.5'  # used when Sphinx builds its own docs
+__version__  = '1.3.5+'
+__released__ = '1.3.5+'  # used when Sphinx builds its own docs
 
 # version info for better programmatic use
 # possible values for 3rd element: 'alpha', 'beta', 'rc', 'final'
 # 'final' has 0 as the last element
-version_info = (1, 3, 5, 'final', 0)
+version_info = (1, 3, 6, 'beta', 1)
 
 package_dir = path.abspath(path.dirname(__file__))
 
