fd674ef9f91216363b273f299bdfcd1fd3b65445
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-02-08 21:12:35 +0900

    Fix #2199: Use ``imagesize`` package to obtain size of images

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ requires = [
     'snowballstemmer>=1.1',
     'babel>=1.3,!=2.0',
     'alabaster>=0.7,<0.8',
+    'imagesize',
 ]
 extras_require = {
     # Environment Marker works for wheel 0.24 or later
