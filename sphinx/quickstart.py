ecf2236724077ce1febdf8c80eed62a018161880
Tony Narlock <tony@git-pull.com>
2016-02-10 09:24:39 -0600

    Add newline below comments in quickstart
    See also: https://git.io/vgaHi

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -92,11 +92,13 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
+#
 # needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -109,10 +111,12 @@ templates_path = ['%(dot)stemplates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
+#
 # source_suffix = ['.rst', '.md']
 source_suffix = '%(suffix)s'
 
 # The encoding of source files.
+#
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
@@ -141,8 +145,11 @@ language = %(language)r
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
+#
 # today = ''
+#
 # Else, today_fmt is used as the format for a strftime call.
+#
 # today_fmt = '%%B %%d, %%Y'
 
 # List of patterns, relative to source directory, that match files and
@@ -152,17 +159,21 @@ exclude_patterns = [%(exclude_patterns)s]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
+#
 # default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
+#
 # add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
+#
 # add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
+#
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
@@ -182,11 +193,13 @@ todo_include_todos = %(ext_todo)s
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+#
 html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+#
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -194,18 +207,22 @@ html_theme = 'alabaster'
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
+#
 # html_title = u'%(project_str)s v%(release_str)s'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
+#
 # html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
+#
 # html_logo = None
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
+#
 # html_favicon = None
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -216,45 +233,57 @@ html_static_path = ['%(dot)sstatic']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
+#
 # html_extra_path = []
 
 # If not None, a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
 # The empty string is equivalent to '%%b %%d, %%Y'.
+#
 # html_last_updated_fmt = None
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
+#
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
+#
 # html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
+#
 # html_additional_pages = {}
 
 # If false, no module index is generated.
+#
 # html_domain_indices = True
 
 # If false, no index is generated.
+#
 # html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
+#
 # html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
+#
 # html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+#
 # html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+#
 # html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
+#
 # html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
@@ -264,15 +293,18 @@ html_static_path = ['%(dot)sstatic']
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr', 'zh'
+#
 # html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # 'ja' uses this config value.
 # 'zh' user can custom change `jieba` dictionary path.
+#
 # html_search_options = {'type': 'default'}
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
+#
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
@@ -282,15 +314,19 @@ htmlhelp_basename = '%(project_fn)sdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
+#
 # 'papersize': 'letterpaper',
 
 # The font size ('10pt', '11pt' or '12pt').
+#
 # 'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
+#
 # 'preamble': '',
 
 # Latex figure (float) alignment
+#
 # 'figure_align': 'htbp',
 }
 
@@ -304,22 +340,28 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
+#
 # latex_logo = None
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
+#
 # latex_use_parts = False
 
 # If true, show page references after internal links.
+#
 # latex_show_pagerefs = False
 
 # If true, show URL addresses after external links.
+#
 # latex_show_urls = False
 
 # Documents to append as an appendix to all manuals.
+#
 # latex_appendices = []
 
 # If false, no module index is generated.
+#
 # latex_domain_indices = True
 
 
@@ -333,6 +375,7 @@ man_pages = [
 ]
 
 # If true, show URL addresses after external links.
+#
 # man_show_urls = False
 
 
@@ -348,15 +391,19 @@ texinfo_documents = [
 ]
 
 # Documents to append as an appendix to all manuals.
+#
 # texinfo_appendices = []
 
 # If false, no module index is generated.
+#
 # texinfo_domain_indices = True
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
+#
 # texinfo_show_urls = 'footnote'
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
+#
 # texinfo_no_detailmenu = False
 '''
 
@@ -377,10 +424,12 @@ epub_copyright = copyright
 # optimized for small screen space, using the same theme for HTML and epub
 # output is usually not wise. This defaults to 'epub', a theme designed to save
 # visual space.
+#
 # epub_theme = 'epub'
 
 # The language of the text. It defaults to the language option
 # or 'en' if the language is not set.
+#
 # epub_language = ''
 
 # The scheme of the identifier. Typical schemes are ISBN or URL.
@@ -388,47 +437,60 @@ epub_copyright = copyright
 
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
+#
 # epub_identifier = ''
 
 # A unique identification for the text.
+#
 # epub_uid = ''
 
 # A tuple containing the cover image and cover page html template filenames.
+#
 # epub_cover = ()
 
 # A sequence of (type, uri, title) tuples for the guide element of content.opf.
+#
 # epub_guide = ()
 
 # HTML files that should be inserted before the pages created by sphinx.
 # The format is a list of tuples containing the path and title.
+#
 # epub_pre_files = []
 
 # HTML files that should be inserted after the pages created by sphinx.
 # The format is a list of tuples containing the path and title.
+#
 # epub_post_files = []
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
 # The depth of the table of contents in toc.ncx.
+#
 # epub_tocdepth = 3
 
 # Allow duplicate toc entries.
+#
 # epub_tocdup = True
 
 # Choose between 'default' and 'includehidden'.
+#
 # epub_tocscope = 'default'
 
 # Fix unsupported image types using the Pillow.
+#
 # epub_fix_images = False
 
 # Scale large images.
+#
 # epub_max_image_width = 0
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
+#
 # epub_show_urls = 'inline'
 
 # If false, no index is generated.
+#
 # epub_use_index = True
 '''
 
