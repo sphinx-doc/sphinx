7b759b1db7c16b894552362cb4a8f8d9355bfc9f
Alex Gaynor <alex.gaynor@gmail.com>
2016-02-13 10:14:16 -0500

    Removed ez_setup.py
    There are no common cases where it will be used anymore:
    
    * pip always installs stuff with setuptools
    * Modern pythons preinstall setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    import ez_setup
-    ez_setup.use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 import os
 import sys
