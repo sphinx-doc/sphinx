bd6978f22124b5f41baf19ce3d6090ccc905dad4
EricFromCanada <eric@lassosoft.com>
2016-02-17 10:33:27 -0500

    make conf.py's html_favicon description match docs

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -202,8 +202,8 @@ html_theme = 'alabaster'
 # of the sidebar.
 #html_logo = None
 
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# The name of an image file (relative to this directory) to use as a favicon of
+# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
 
