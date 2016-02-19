606daeda18c81e5bb844025da5cffa02ed8fa0f4
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-02-19 23:35:10 +0900

    Fix the confval type checker emits warnings if unicode is given to confvals which expects string value

--- tests/roots/test-config/conf.py
+++ tests/roots/test-config/conf.py
@@ -1,3 +1,5 @@
+from sphinx.config import string_classes
+
 value1 = 123  # wrong type
 value2 = 123  # lambda with wrong type
 value3 = []  # lambda with correct type
@@ -18,6 +20,13 @@ value8 = C()  # sibling type
 # both have no default or permissible types
 value9 = 'foo'
 value10 = 123
+value11 = u'bar'
+value12 = u'bar'
+value13 = 'bar'
+value14 = u'bar'
+value15 = 'bar'
+value16 = u'bar'
+
 
 def setup(app):
     app.add_config_value('value1', 'string', False)
@@ -30,3 +39,9 @@ def setup(app):
     app.add_config_value('value8', B(), False)
     app.add_config_value('value9', None, False)
     app.add_config_value('value10', None, False)
+    app.add_config_value('value11', None, False, [str])
+    app.add_config_value('value12', 'string', False)
+    app.add_config_value('value13', None, False, string_classes)
+    app.add_config_value('value14', None, False, string_classes)
+    app.add_config_value('value15', u'unicode', False)
+    app.add_config_value('value16', u'unicode', False)
