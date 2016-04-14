bfd0dd5c3ae97f4d07a25ba4f8fb77fba866f202
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-04-15 01:42:21 +0900

    Fix pep8 violation (F401) on conf.py

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -86,13 +86,12 @@ QUICKSTART_CONF += u'''\
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import os
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+# import os
+# import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
