53c875223b19d15bd23be5b9851b7afabdc67cf4
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-05-21 00:35:48 +0900

    Fix #2512: generated Makefile causes BSD make crashed if sphinx-build not found

--- sphinx/quickstart.py
+++ sphinx/quickstart.py
@@ -534,16 +534,6 @@ SPHINXBUILD   = sphinx-build
 PAPER         =
 BUILDDIR      = %(rbuilddir)s
 
-# User-friendly check for sphinx-build
-ifeq ($(shell which $(SPHINXBUILD) >/dev/null 2>&1; echo $$?), 1)
-\t$(error \
-The '$(SPHINXBUILD)' command was not found. Make sure you have Sphinx \
-installed, then set the SPHINXBUILD environment variable to point \
-to the full path of the '$(SPHINXBUILD)' executable. Alternatively you \
-can add the directory with the executable to your PATH. \
-If you don\\'t have Sphinx installed, grab it from http://sphinx-doc.org/)
-endif
-
 # Internal variables.
 PAPEROPT_a4     = -D latex_paper_size=a4
 PAPEROPT_letter = -D latex_paper_size=letter
@@ -1060,16 +1050,6 @@ SPHINXBUILD   = sphinx-build
 SPHINXPROJ    = %(project_fn)s
 SOURCEDIR     = %(rsrcdir)s
 BUILDDIR      = %(rbuilddir)s
-
-# User-friendly check for sphinx-build.
-ifeq ($(shell which $(SPHINXBUILD) >/dev/null 2>&1; echo $$?), 1)
-$(error \
-The '$(SPHINXBUILD)' command was not found. Make sure you have Sphinx \
-installed, then set the SPHINXBUILD environment variable to point \
-to the full path of the '$(SPHINXBUILD)' executable. Alternatively you \
-can add the directory with the executable to your PATH. \
-If you don't have Sphinx installed, grab it from http://sphinx-doc.org/)
-endif
 
 # Has to be explicit, otherwise we don't get "make" without targets right.
 help:
