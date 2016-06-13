292c6d4e95337549d81c45390bd0a2656aa4f741
jfbu <jfbu@free.fr>
2016-06-13 15:07:17 +0200

    Revert notice->sphinxnotice change in latex writer

--- sphinx/writers/latex.py
+++ sphinx/writers/latex.py
@@ -1525,19 +1525,19 @@ class LaTeXTranslator(nodes.NodeVisitor):
         self.body.append('}')
 
     def visit_admonition(self, node):
-        self.body.append('\n\\begin{sphinxnotice}{note}')
+        self.body.append('\n\\begin{notice}{note}')
 
     def depart_admonition(self, node):
-        self.body.append('\\end{sphinxnotice}\n')
+        self.body.append('\\end{notice}\n')
 
     def _make_visit_admonition(name):
         def visit_admonition(self, node):
-            self.body.append(u'\n\\begin{sphinxnotice}{%s}{%s:}' %
+            self.body.append(u'\n\\begin{notice}{%s}{%s:}' %
                              (name, admonitionlabels[name]))
         return visit_admonition
 
     def _depart_named_admonition(self, node):
-        self.body.append('\\end{sphinxnotice}\n')
+        self.body.append('\\end{notice}\n')
 
     visit_attention = _make_visit_admonition('attention')
     depart_attention = _depart_named_admonition
