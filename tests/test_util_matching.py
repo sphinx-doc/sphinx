829b5631a34a7da67e6dee18853c82c12e1aa59d
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-07-03 15:11:15 +0900

    Add testcase for compile_matchers()

---
+++ tests/test_util_matching.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+    test_util_matching
+    ~~~~~~~~~~~~~~~~~~
+
+    Tests sphinx.util.matching functions.
+
+    :copyright: Copyright 2007-2016 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+from sphinx.util.matching import compile_matchers
+
+
+def test_compile_matchers():
+    # exact matching
+    pat = compile_matchers(['hello.py']).pop()
+    assert pat('hello.py')
+    assert not pat('hello-py')
+    assert not pat('subdir/hello.py')
+
+    # wild card (*)
+    pat = compile_matchers(['hello.*']).pop()
+    assert pat('hello.py')
+    assert pat('hello.rst')
+
+    pat = compile_matchers(['*.py']).pop()
+    assert pat('hello.py')
+    assert pat('world.py')
+    assert not pat('subdir/hello.py')
+
+    # wild card (**)
+    pat = compile_matchers(['hello.**']).pop()
+    assert pat('hello.py')
+    assert pat('hello.rst')
+    assert pat('hello.py/world.py')
+
+    pat = compile_matchers(['**.py']).pop()
+    assert pat('hello.py')
+    assert pat('world.py')
+    assert pat('subdir/hello.py')
+
+    pat = compile_matchers(['**/hello.py']).pop()
+    assert not pat('hello.py')
+    assert pat('subdir/hello.py')
+    assert pat('subdir/subdir/hello.py')
+
+    # wild card (?)
+    pat = compile_matchers(['hello.?']).pop()
+    assert pat('hello.c')
+    assert not pat('hello.py')
+
+    # pattern ([...])
+    pat = compile_matchers(['hello[12\\].py']).pop()
+    assert pat('hello1.py')
+    assert pat('hello2.py')
+    assert pat('hello\\.py')
+    assert not pat('hello3.py')
+
+    pat = compile_matchers(['hello[^12].py']).pop()  # "^" is not negative identifier
+    assert pat('hello1.py')
+    assert pat('hello2.py')
+    assert pat('hello^.py')
+    assert not pat('hello3.py')
+
+    # negative pattern ([!...])
+    pat = compile_matchers(['hello[!12].py']).pop()
+    assert not pat('hello1.py')
+    assert not pat('hello2.py')
+    assert not pat('hello/.py')  # negative pattern does not match to "/"
+    assert pat('hello3.py')
+
+    # non patterns
+    pat = compile_matchers(['hello[.py']).pop()
+    assert pat('hello[.py')
+    assert not pat('hello.py')
+
+    pat = compile_matchers(['hello[].py']).pop()
+    assert pat('hello[].py')
+    assert not pat('hello.py')
+
+    pat = compile_matchers(['hello[!].py']).pop()
+    assert pat('hello[!].py')
+    assert not pat('hello.py')
