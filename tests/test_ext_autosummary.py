13c63cb17393acde35474f95b82a7050b0889342
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-07-14 21:07:33 +0900

    Optimize tests: test_get_items_summary

--- tests/test_ext_autosummary.py
+++ tests/test_ext_autosummary.py
@@ -54,7 +54,7 @@ def test_mangle_signature():
         assert res == outp, (u"'%s' -> '%s' != '%s'" % (inp, res, outp))
 
 
-@with_app(buildername='html', **default_kw)
+@with_app(buildername='dummy', **default_kw)
 def test_get_items_summary(app, status, warning):
     # monkey-patch Autosummary.get_items so we can easily get access to it's
     # results..
