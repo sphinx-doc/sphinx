bc6b4bf70e19055b9d855c24c97b5674f70d69fd
Takeshi KOMIYA <i.tkomiya@gmail.com>
2016-10-02 06:58:55 +0900

    Remove unused LaTeX macro filename from tests

--- tests/test_build_latex.py
+++ tests/test_build_latex.py
@@ -27,8 +27,8 @@ from test_build_html import ENV_WARNINGS
 
 LATEX_ENGINES = ['pdflatex', 'lualatex', 'xelatex']
 DOCCLASSES = ['howto', 'manual']
-STYLEFILES = ['article.sty', 'fancyhdr.sty', 'fancybox.sty', 'titlesec.sty', 'amsmath.sty',
-              'framed.sty', 'color.sty', 'fancyvrb.sty', 'threeparttable.sty']
+STYLEFILES = ['article.sty', 'fancyhdr.sty', 'titlesec.sty', 'amsmath.sty', 'framed.sty',
+              'color.sty', 'fancyvrb.sty', 'threeparttable.sty']
 
 LATEX_WARNINGS = ENV_WARNINGS + """\
 %(root)s/index.rst:\\d+: WARNING: unknown option: &option
