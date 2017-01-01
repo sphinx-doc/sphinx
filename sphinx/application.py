54cac94cdfa0f558d87dfdc5b36618aa6dda61df
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-01-01 20:07:16 +0900

    Fix #3294: ``add_latex_package()`` make crashes non-LaTeX builders

--- sphinx/application.py
+++ sphinx/application.py
@@ -806,7 +806,8 @@ class Sphinx(object):
 
     def add_latex_package(self, packagename, options=None):
         self.debug('[app] adding latex package: %r', packagename)
-        self.builder.usepackages.append((packagename, options))
+        if hasattr(self.builder, 'usepackages'):  # only for LaTeX builder
+            self.builder.usepackages.append((packagename, options))
 
     def add_lexer(self, alias, lexer):
         self.debug('[app] adding lexer: %r', (alias, lexer))
