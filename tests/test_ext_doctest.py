e7166bbc08ff0d8572763364685993b7ec48f6e1
Marco Buttu <marco.buttu@gmail.com>
2017-01-15 21:13:34 +0100

    Removed useless import

--- tests/test_ext_doctest.py
+++ tests/test_ext_doctest.py
@@ -8,7 +8,6 @@
     :copyright: Copyright 2007-2016 by the Sphinx team, see AUTHORS.
     :license: BSD, see LICENSE for details.
 """
-import platform
 import pytest
 from sphinx.ext.doctest import compare_version
 
