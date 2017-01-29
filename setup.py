14a864a63700668cfe84565bcef0dd26846afabb
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-01-29 13:15:49 +0900

    Support requests-2.0.0 (experimental) (refs: #3367)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ requires = [
     'babel>=1.3,!=2.0',
     'alabaster>=0.7,<0.8',
     'imagesize',
-    'requests>=2.4.0',
+    'requests>=2.0.0',
 ]
 extras_require = {
     # Environment Marker works for wheel 0.24 or later
