99315fa8e0b1df7165504cccbd4571a687389bc7
jfbu <jfbu@free.fr>
2017-02-18 16:26:00 +0100

    update root footnote test

--- tests/test_build_latex.py
+++ tests/test_build_latex.py
@@ -480,11 +480,15 @@ def test_footnote(app, status, warning):
             '{\\phantomsection\\label{\\detokenize{footnote:bar}} '
             '\ncite\n}') in result
     assert '\\caption{Table caption \\sphinxfootnotemark[4]' in result
-    assert ('\\begin{footnotetext}[4]\\sphinxAtStartFootnote\n'
-            'footnotes in table caption\n%\n\\end{footnotetext}\\ignorespaces %\n'
+    assert ('\\hline%\n\\begin{footnotetext}[4]\\sphinxAtStartFootnote\n'
+            'footnote in table caption\n%\n\\end{footnotetext}\\ignorespaces %\n'
             '\\begin{footnotetext}[5]\\sphinxAtStartFootnote\n'
-            'footnotes in table\n%\n\\end{footnotetext}\\ignorespaces ') in result
-    assert '\\end{threeparttable}\n\\par\n\\end{savenotes}\n' in result
+            'footnote in table header\n%\n\\end{footnotetext}\\ignorespaces \n'
+            'VIDIOC\\_CROPCAP\n&\n') in result
+    assert ('Information about VIDIOC\\_CROPCAP %\n'
+            '\\begin{footnote}[6]\\sphinxAtStartFootnote\n'
+            'footnote in table not in header\n%\n\\end{footnote}\n\\\\\n\hline\n'
+            '\\end{tabulary}\n\\end{threeparttable}\n\\par\n\\end{savenotes}\n') in result
 
 
 @pytest.mark.sphinx('latex', testroot='footnotes')
