2a4a6d0cdaad38ac57f07d6f216db38ab13177e9
Lilian Besson <Naereen@users.noreply.github.com>
2017-03-10 22:56:36 +0100

    Union[unicode, None] → unicode

--- sphinx/util/__init__.py
+++ sphinx/util/__init__.py
@@ -281,7 +281,7 @@ def get_module_source(modname):
 
 
 def get_full_modname(modname, attribute):
-    # type: (str, unicode) -> Union[unicode, NoneType]
+    # type: (str, unicode) -> unicode
     if modname is None:
         # Prevents a TypeError: if the last getattr() call will return None
         # then it's better to return it directly
