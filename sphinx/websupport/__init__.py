a62104ccda6070fe04ba4d2d7ba6969b7767a1fc
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-04-22 17:53:47 +0900

    Emit deprecation warnings on importing sphinx.websupport

--- sphinx/websupport/__init__.py
+++ sphinx/websupport/__init__.py
@@ -9,7 +9,14 @@
     :license: BSD, see LICENSE for details.
 """
 
+import warnings
+
+from sphinx.deprecation import RemovedInSphinx20Warning
 from sphinxcontrib.websupport import WebSupport  # NOQA
 from sphinxcontrib.websupport import errors  # NOQA
 from sphinxcontrib.websupport.search import BaseSearch, SEARCH_ADAPTERS  # NOQA
 from sphinxcontrib.websupport.storage import StorageBackend  # NOQA
+
+warnings.warn('sphinx.websupport module is now provided as sphinxcontrib.webuspport. '
+              'sphinx.websupport will be removed in Sphinx-2.0.  Please use it instaed',
+              RemovedInSphinx20Warning)
