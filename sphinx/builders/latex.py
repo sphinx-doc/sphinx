9f5f169af25bfcf489c2a04d71046cafd53b220c
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-05-07 16:36:29 +0900

    Update warning message (refs: #3524)

--- sphinx/builders/latex.py
+++ sphinx/builders/latex.py
@@ -239,7 +239,8 @@ def validate_config_values(app):
         if app.config.latex_elements.get('preamble'):
             app.warn("latex_preamble conflicts with latex_elements['preamble'], ignored.")
         else:
-            app.warn("latex_preamble is deprecated. Use latex_elements['preamble'] instead.")
+            app.warn("latex_preamble is deprecated. "
+                     "Define a latex_elements dict with a 'preamble' key.")
             app.config.latex_elements['preamble'] = app.config.latex_preamble
 
     if app.config.latex_paper_size != 'letter':
@@ -247,7 +248,7 @@ def validate_config_values(app):
             app.warn("latex_paper_size conflicts with latex_elements['papersize'], ignored.")
         else:
             app.warn("latex_paper_size is deprecated. "
-                     "Use latex_elements['papersize'] instead.")
+                     "Define a latex_elements dict with a 'papersize' key.")
             if app.config.latex_paper_size:
                 app.config.latex_elements['papersize'] = app.config.latex_paper_size + 'paper'
 
@@ -255,7 +256,8 @@ def validate_config_values(app):
         if app.config.latex_elements.get('pointsize'):
             app.warn("latex_font_size conflicts with latex_elements['pointsize'], ignored.")
         else:
-            app.warn("latex_font_size is deprecated. Use latex_elements['pointsize'] instead.")
+            app.warn("latex_font_size is deprecated. "
+                     "Define a latex_elements dict with a 'pointsize' key.")
             app.config.latex_elements['pointsize'] = app.config.latex_font_size
 
     if 'footer' in app.config.latex_elements:
