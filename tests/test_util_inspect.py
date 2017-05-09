5e653164596b940a175555160ec0d26f35d75bd3
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-05-09 23:02:40 +0900

    Skip testcase for sphinx.util.inspect on 3.4.3
    The inspect library of python-3.4.3 also can't handle wrapped
    functions.

--- tests/test_util_inspect.py
+++ tests/test_util_inspect.py
@@ -10,6 +10,7 @@
 """
 from unittest import TestCase
 
+import sys
 from six import PY3
 import functools
 from textwrap import dedent
@@ -65,7 +66,7 @@ class TestGetArgSpec(TestCase):
             pass
 
         assert expected_unbound == inspect.getargspec(Foo.method)
-        if PY3:
+        if PY3 and sys.version_info >= (3, 4, 4):
             # On py2, the inspect functions don't properly handle bound
             # methods (they include a spurious 'self' argument)
             assert expected_bound == inspect.getargspec(bound_method)
