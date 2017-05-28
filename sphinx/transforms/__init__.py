0b945ce15793d72ec7c10ffd687f8c221985a7bf
Takeshi KOMIYA <i.tkomiya@gmail.com>
2017-05-28 22:47:57 +0900

    Fix #3817: latex builder raises AttributeError

--- sphinx/transforms/__init__.py
+++ sphinx/transforms/__init__.py
@@ -87,7 +87,7 @@ class SphinxTransformer(Transformer):
     def apply_transforms(self):
         # type: () -> None
         if isinstance(self.document, nodes.document):
-            if hasattr(self.document.settings, 'env') and self.env:
+            if not hasattr(self.document.settings, 'env') and self.env:
                 self.document.settings.env = self.env
 
             Transformer.apply_transforms(self)
