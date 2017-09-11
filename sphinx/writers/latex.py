8542e29b40787676630fd1b77258826c0aa747a6
jfbu <jfbu@free.fr>
2017-09-11 20:35:25 +0200

    Fix flake8 line too long violation in latex.py

--- sphinx/writers/latex.py
+++ sphinx/writers/latex.py
@@ -1463,7 +1463,8 @@ class LaTeXTranslator(nodes.NodeVisitor):
         if cell.width > 1 or cell.height > 1:
             self.body.append('\\begin{varwidth}[t]{\\sphinxcolwidth{%d}{%d}}\n'
                              % (cell.width, self.table.colcount))
-            context = ('\\par\n\\vskip-\\baselineskip\\vbox{\\hbox{\\strut}}\\end{varwidth}%\n') + context
+            context = ('\\par\n\\vskip-\\baselineskip'
+                       '\\vbox{\\hbox{\\strut}}\\end{varwidth}%\n') + context
             self.needs_linetrimming = 1
         if len(node) > 2 and len(node.astext().split('\n')) > 2:
             self.needs_linetrimming = 1
