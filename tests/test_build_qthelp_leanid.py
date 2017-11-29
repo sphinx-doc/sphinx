15067a28be73248923d13b94f3d1cf039cf27719
Joerg Kreuzberger <j.kreuzberger@procitec.de>
2017-11-29 17:32:38 +0100

    [1491] Adding tests for option qthelp_lean_id

---
+++ tests/test_build_qthelp_leanid.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    test_build_qthelp_leanid
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Test the Qt Help builder and check its output.  We don't need to
+    test the HTML itself; that's already handled by
+    :file:`test_build_html.py`.
+
+    :copyright: Copyright 2007-2017 by the Sphinx team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+import pytest
+
+
+@pytest.mark.sphinx('qthelp', testroot='index')
+def test_qthelp_namespace(app, status, warning):
+    # default namespace
+    app.builder.build_all()
+    qhp = (app.outdir / 'Python.qhp').text()
+    assert '<keyword name="features (ID_FEATURE)" id="ID_FEATURE.features"' in qhp
+
+    # give a namespace
+    app.config.qthelp_id_lean = True
+    app.builder.build_all()
+    qhp = (app.outdir / 'Python.qhp').text()
+    assert '<keyword name="features" id="ID_FEATURE"' in qhp
