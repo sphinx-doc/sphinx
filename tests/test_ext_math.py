f91c7327494a8dab1e1bcc985fcadb0cc6cb5ebf
Takeshi KOMIYA <i.tkomiya@gmail.com>
2018-01-19 23:12:25 +0900

    Fix #4438: math: math with labels with whitespace cause html error

--- tests/test_ext_math.py
+++ tests/test_ext_math.py
@@ -27,9 +27,9 @@ def test_jsmath(app, status, warning):
     assert (u'<span class="eqno">(1)<a class="headerlink" href="#equation-foo" '
             u'title="Permalink to this equation">\xb6</a></span>'
             u'<div class="math" id="equation-foo">\ne^{i\\pi} = 1</div>' in content)
-    assert (u'<span class="eqno">(2)<a class="headerlink" href="#equation-math:0" '
+    assert (u'<span class="eqno">(2)<a class="headerlink" href="#equation-math-0" '
             u'title="Permalink to this equation">\xb6</a></span>'
-            u'<div class="math" id="equation-math:0">\n'
+            u'<div class="math" id="equation-math-0">\n'
             u'e^{ix} = \\cos x + i\\sin x</div>' in content)
     assert '<div class="math">\nn \\in \\mathbb N</div>' in content
     assert '<div class="math">\na + 1 &lt; b</div>' in content
@@ -85,7 +85,7 @@ def test_math_number_all_mathjax(app, status, warning):
     app.builder.build_all()
 
     content = (app.outdir / 'index.html').text()
-    html = (r'<div class="math" id="equation-index:0">\s*'
+    html = (r'<div class="math" id="equation-index-0">\s*'
             r'<span class="eqno">\(1\)<a .*>\xb6</a></span>\\\[a\^2\+b\^2=c\^2\\\]</div>')
     assert re.search(html, content, re.S)
 
