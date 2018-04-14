aa21d78e42ebbc993f4f5df66755a044a95d52c8
jfbu <jfbu@free.fr>
2018-04-14 19:48:24 +0200

    Fix again #4837, using better equivalent to deprecated \sf

--- sphinx/writers/latex.py
+++ sphinx/writers/latex.py
@@ -615,9 +615,10 @@ class LaTeXTranslator(nodes.NodeVisitor):
         if builder.config.language \
            and 'fncychap' not in builder.config.latex_elements:
             # use Sonny style if any language specified
-            self.elements['fncychap'] = ('\\usepackage[Sonny]{fncychap}'
-                                         '\\ChNameVar{\\Large\\sffamily}'
-                                         '\\ChTitleVar{\\Large\\sffamily}')
+            self.elements['fncychap'] = ('\\usepackage[Sonny]{fncychap}\n'
+                                         '\\ChNameVar{\\Large\\normalfont'
+                                         '\\sffamily}\n\\ChTitleVar{\\Large'
+                                         '\\normalfont\\sffamily}')
 
         self.babel = ExtBabel(builder.config.language)
         if builder.config.language and not self.babel.is_supported_language():
