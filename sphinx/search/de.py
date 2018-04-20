02ef4e64a7e3e2da70e5a75daf157b1124b57cab
Dennis Wegner <dennis@instant-thinking.de>
2018-04-20 17:30:13 +0200

    Revert "Fixed the HTML search for uppercase characters in the German language file"
    This reverts commit 4e32df4e96010c6cfe725fe4d39f0ee59f8925db.

--- sphinx/search/de.py
+++ sphinx/search/de.py
@@ -318,4 +318,4 @@ class SearchGerman(SearchLanguage):
 
     def stem(self, word):
         # type: (unicode) -> unicode
-        return self.stemmer.stemWord(word.lower())
+        return self.stemmer.stemWord(word)
