acb7edcae85318e373725fd003e3609fdef71d79
jfbu <jfbu@free.fr>
2018-04-25 10:26:12 +0200

    Fix typos in a docstring (refs: #4901)

--- sphinx/util/logging.py
+++ sphinx/util/logging.py
@@ -62,13 +62,13 @@ def getLogger(name):
     # type: (str) -> SphinxLoggerAdapter
     """Get logger wrapped by :class:`sphinx.util.logging.SphinxLoggerAdapter`.
 
-    Sphinx logger always uses ``sphinx.*`` namesapce to be independent from
-    settings of root logger.  It ensure logging is consistent even if a
+    Sphinx logger always uses ``sphinx.*`` namespace to be independent from
+    settings of root logger.  It ensures logging is consistent even if a
     third-party extension or imported application resets logger settings.
 
     Example usage::
 
-        >>> from sphinx.utils import logging
+        >>> from sphinx.util import logging
         >>> logger = logging.getLogger(__name__)
         >>> logger.info('Hello, this is an extension!')
         Hello, this is an extension!
