a8a3bb796e93b9fa0825bc01867197bb95009af8
travis-ci <travis@travis-ci.org>
2018-05-01 21:44:03 +0200

    Display a warning in Highlight directive if linenothreshold value is not valid and coerce exception cacthing to ValueError instead of a general Exception.

--- sphinx/directives/code.py
+++ sphinx/directives/code.py
@@ -53,8 +53,14 @@ class Highlight(SphinxDirective):
         if 'linenothreshold' in self.options:
             try:
                 linenothreshold = int(self.options['linenothreshold'])
-            except Exception:
-                linenothreshold = 10
+            except ValueError:
+                default_linenothreshold = 10
+                location = self.state_machine.get_source_and_line(self.lineno)
+                logger.warning(__('linenothreshold value must be int typed. '
+                                  'Passed %r. Using default value (10).'
+                                  % self.options['linenothreshold']),
+                              location=location)
+                linenothreshold = default_linenothreshold
         else:
             linenothreshold = sys.maxsize
         return [addnodes.highlightlang(lang=self.arguments[0].strip(),
