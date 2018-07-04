e7ee03505047d11c8c8e08e3c10821fe8db97ff8
jfbu <jfbu@free.fr>
2018-07-04 08:37:57 +0200

    LaTeX use ``\sphinxstyleindexletterhead`` also for domain indices

--- sphinx/writers/latex.py
+++ sphinx/writers/latex.py
@@ -862,8 +862,7 @@ class LaTeXTranslator(nodes.NodeVisitor):
         def generate(content, collapsed):
             # type: (List[Tuple[unicode, List[Tuple[unicode, unicode, unicode, unicode, unicode]]]], bool) -> None  # NOQA
             ret.append('\\begin{sphinxtheindex}\n')
-            ret.append('\\def\\bigletter#1{{\\Large\\sffamily#1}'
-                       '\\nopagebreak\\vspace{1mm}}\n')
+            ret.append('\\let\\bigletter\\sphinxstyleindexletterhead\n')
             for i, (letter, entries) in enumerate(content):
                 if i > 0:
                     ret.append('\\indexspace\n')
