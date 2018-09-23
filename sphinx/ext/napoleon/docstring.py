sphinx-doc/sphinx

commit bd1e33643cdf6196befd0a1ef3064a06f09d0422

Qualify attribute names for generated ivar (#5129)
* Use qualified name in ivar

* bugfix

* fix style

* Moves _qualify_name into class to be more consistent with surrounding code

sphinx/ext/napoleon/docstring.py
@@ -608,6 +608,7 @@ class GoogleDocstring(UnicodeMixin):
         lines = []
         for _name, _type, _desc in self._consume_fields():
             if self._config.napoleon_use_ivar:
+                _name = self._qualify_name(_name, self._obj)
                 field = ':ivar %s: ' % _name  # type: unicode
                 lines.extend(self._format_block(field, _desc))
                 if _type:
@@ -783,6 +784,18 @@ class GoogleDocstring(UnicodeMixin):
         return ("".join(before_colon).strip(),
                 colon,
                 "".join(after_colon).strip())
+
+    def _qualify_name(self, attr_name, klass):
+        # type: (unicode, type) -> unicode
+        if klass and '.' not in attr_name:
+            if attr_name.startswith('~'):
+                attr_name = attr_name[1:]
+            try:
+                q = klass.__qualname__
+            except AttributeError:
+                q = klass.__name__
+            return '~%s.%s' % (q, attr_name)
+        return attr_name
 
     def _strip_empty(self, lines):
         # type: (List[unicode]) -> List[unicode]

commit 408219cf81c1bac0c31684ca8fe36a4cb8c88cfc

Fixes mypy type errors introduced by #5129

sphinx/ext/napoleon/docstring.py
@@ -11,9 +11,9 @@
     :license: BSD, see LICENSE for details.
 """
 
-import collections.abc
 import inspect
 import re
+from collections.abc import Callable
 from functools import partial
 
 from six import string_types, u
@@ -25,7 +25,7 @@ from sphinx.util.pycompat import UnicodeMixin
 
 if False:
     # For type annotation
-    from typing import Any, Callable, Dict, List, Tuple, Union  # NOQA
+    from typing import Any, Dict, List, Tuple, Type, Union  # NOQA
     from sphinx.application import Sphinx  # NOQA
     from sphinx.config import Config as SphinxConfig  # NOQA
 
@@ -124,7 +124,7 @@ class GoogleDocstring(UnicodeMixin):
                 what = 'class'
             elif inspect.ismodule(obj):
                 what = 'module'
-            elif isinstance(obj, collections.abc.Callable):
+            elif isinstance(obj, Callable):
                 what = 'function'
             else:
                 what = 'object'
@@ -787,7 +787,7 @@ class GoogleDocstring(UnicodeMixin):
                 "".join(after_colon).strip())
 
     def _qualify_name(self, attr_name, klass):
-        # type: (unicode, type) -> unicode
+        # type: (unicode, Type) -> unicode
         if klass and '.' not in attr_name:
             if attr_name.startswith('~'):
                 attr_name = attr_name[1:]