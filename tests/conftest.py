0137b0287a1fb57d00b33bac57bdf3c117477475
Jon Dufresne <jon.dufresne@gmail.com>
2018-11-12 14:31:29 -0800

    Remove dead code `or '.'`
    The expression `os.path.dirname(__file__)` always evaluates as a truthy
    value. The `or '.'` is never evaluated.

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,7 +24,7 @@ collect_ignore = ['roots']
 
 @pytest.fixture(scope='session')
 def rootdir():
-    return path(os.path.dirname(__file__) or '.').abspath() / 'roots'
+    return path(os.path.dirname(__file__)).abspath() / 'roots'
 
 
 def pytest_report_header(config):
