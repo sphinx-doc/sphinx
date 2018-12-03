b76966815ce7848b0ef08e136f28782e010c1417
Takeshi KOMIYA <i.tkomiya@gmail.com>
2018-12-03 21:10:26 +0900

    Fix annotations of testing

--- sphinx/testing/util.py
+++ sphinx/testing/util.py
@@ -65,10 +65,14 @@ def assert_node(node, cls=None, xpath="", **kwargs):
                 if isinstance(cls[1], tuple):
                     assert_node(node, cls[1], xpath=xpath, **kwargs)
                 else:
+                    assert isinstance(node, nodes.Element), \
+                        'The node%s does not have any children' % xpath
                     assert len(node) == 1, \
                         'The node%s has %d child nodes, not one' % (xpath, len(node))
                     assert_node(node[0], cls[1:], xpath=xpath + "[0]", **kwargs)
         elif isinstance(cls, tuple):
+            assert isinstance(node, nodes.Element), \
+                'The node%s does not have any items' % xpath
             assert len(node) == len(cls), \
                 'The node%s has %d child nodes, not %r' % (xpath, len(node), len(cls))
             for i, nodecls in enumerate(cls):
@@ -80,10 +84,15 @@ def assert_node(node, cls=None, xpath="", **kwargs):
             assert isinstance(node, cls), \
                 'The node%s is not subclass of %r: %r' % (xpath, cls, node)
 
-    for key, value in kwargs.items():
-        assert key in node, 'The node%s does not have %r attribute: %r' % (xpath, key, node)
-        assert node[key] == value, \
-            'The node%s[%s] is not %r: %r' % (xpath, key, value, node[key])
+    if kwargs:
+        assert isinstance(node, nodes.Element), \
+            'The node%s does not have any attributes' % xpath
+
+        for key, value in kwargs.items():
+            assert key in node, \
+                'The node%s does not have %r attribute: %r' % (xpath, key, node)
+            assert node[key] == value, \
+                'The node%s[%s] is not %r: %r' % (xpath, key, value, node[key])
 
 
 def etree_parse(path):
