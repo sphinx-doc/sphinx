47058cd1756ae8b4a0d0536d4b84ec628ade9099
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-02-09 14:44:37 +0900

    test: Give freshenv=True to test_docutilsconf for windows environment

--- tests/test_docutilsconf.py
+++ tests/test_docutilsconf.py
@@ -15,7 +15,7 @@ from sphinx.testing.util import assert_node
 from sphinx.util.docutils import patch_docutils
 
 
-@pytest.mark.sphinx('dummy', testroot='docutilsconf')
+@pytest.mark.sphinx('dummy', testroot='docutilsconf', freshenv=True)
 def test_html_with_default_docutilsconf(app, status, warning):
     with patch_docutils(app.confdir):
         app.build()
@@ -25,7 +25,7 @@ def test_html_with_default_docutilsconf(app, status, warning):
                                                   [nodes.footnote_reference, "1"])])
 
 
-@pytest.mark.sphinx('dummy', testroot='docutilsconf',
+@pytest.mark.sphinx('dummy', testroot='docutilsconf', freshenv=True,
                     docutilsconf=('[restructuredtext parser]\n'
                                   'trim_footnote_reference_space: true\n'))
 def test_html_with_docutilsconf(app, status, warning):
