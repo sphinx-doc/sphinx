08cd23a0bbc060a6135cd5acc1411693159df8b5
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-02-17 18:22:12 +0900

    Fix #6047: autodoc: ``autofunction`` emits a warning for method objects

--- sphinx/ext/autodoc/__init__.py
+++ sphinx/ext/autodoc/__init__.py
@@ -1056,6 +1056,7 @@ class FunctionDocumenter(DocstringSignatureMixin, ModuleLevelDocumenter):  # typ
             return None
         try:
             if (not isfunction(self.object) and
+                    not inspect.ismethod(self.object) and
                     not isbuiltin(self.object) and
                     not inspect.isclass(self.object) and
                     hasattr(self.object, '__call__')):
