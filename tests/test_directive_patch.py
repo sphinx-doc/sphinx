e5881c23449f8fd78c87796897d42c7cb6b9037b
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-03-05 00:41:08 +0900

    Add testcase for math directive

--- tests/test_directive_patch.py
+++ tests/test_directive_patch.py
@@ -52,3 +52,35 @@ def test_code_directive(app):
     doctree = restructuredtext.parse(app, text)
     assert_node(doctree, [nodes.document, nodes.literal_block, 'print("hello world")'])
     assert_node(doctree[0], language="python", linenos=True, highlight_args={'linenostart': 5})
+
+
+def test_math_directive(app):
+    # normal case
+    text = '.. math:: E = mc^2'
+    doctree = restructuredtext.parse(app, text)
+    assert_node(doctree, [nodes.document, nodes.math_block, 'E = mc^2\n\n'])
+
+    # :name: option
+    text = ('.. math:: E = mc^2\n'
+            '   :name: eq1\n')
+    doctree = restructuredtext.parse(app, text)
+    assert_node(doctree, [nodes.document, (nodes.target,
+                                           [nodes.math_block, "E = mc^2\n\n"])])
+    assert_node(doctree[1], nodes.math_block, docname='index', label="eq1", number=1)
+
+    # :label: option
+    text = ('.. math:: E = mc^2\n'
+            '   :label: eq2\n')
+    doctree = restructuredtext.parse(app, text)
+    assert_node(doctree, [nodes.document, (nodes.target,
+                                           [nodes.math_block, 'E = mc^2\n\n'])])
+    assert_node(doctree[1], nodes.math_block, docname='index', label="eq2", number=2)
+
+    # :label: option without value
+    text = ('.. math:: E = mc^2\n'
+            '   :label:\n')
+    doctree = restructuredtext.parse(app, text)
+    assert_node(doctree, [nodes.document, (nodes.target,
+                                           [nodes.math_block, 'E = mc^2\n\n'])])
+    assert_node(doctree[1], nodes.math_block, ids=['equation-index-0'],
+                docname='index', label="index:0", number=3)
