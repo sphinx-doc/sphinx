f672e00c089947ff97dcf8d5cd85c86495d231d6
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-06-09 13:02:20 +0900

    refactor: correct interface of directive() and role() to docutils'

--- sphinx/util/typing.py
+++ sphinx/util/typing.py
@@ -27,7 +27,7 @@ NoneType = type(None)
 PathMatcher = Callable[[str], bool]
 
 # common role functions
-RoleFunction = Callable[[str, str, str, int, Inliner, Dict, List[str]],
+RoleFunction = Callable[[str, str, str, int, Inliner, Dict[str, Any], List[str]],
                         Tuple[List[nodes.Node], List[nodes.system_message]]]
 
 # title getter functions for enumerable nodes (see sphinx.domains.std)
