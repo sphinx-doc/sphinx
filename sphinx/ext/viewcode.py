108d35a201ec883318a21f4b361df6afe0da129b
Walter Doerwald <walter@livinglogic.de>
2019-07-08 18:27:07 +0200

    Rebuild html when viecode_source_html changes.

--- sphinx/ext/viewcode.py
+++ sphinx/ext/viewcode.py
@@ -241,7 +241,7 @@ def setup(app):
     app.add_config_value('viewcode_import', None, False)
     app.add_config_value('viewcode_enable_epub', False, False)
     app.add_config_value('viewcode_follow_imported_members', True, False)
-    app.add_config_value('viewcode_source_html', None, False)
+    app.add_config_value('viewcode_source_html', None, 'html')
     app.connect('doctree-read', doctree_read)
     app.connect('env-merge-info', env_merge_info)
     app.connect('html-collect-pages', collect_pages)
