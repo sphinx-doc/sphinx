63757596b14991801ce09d1ac9bff425715e0c40
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-08-19 00:35:51 +0900

    Bump version

--- sphinx/__init__.py
+++ sphinx/__init__.py
@@ -32,8 +32,8 @@ if 'PYTHONWARNINGS' not in os.environ:
 warnings.filterwarnings('ignore', "'U' mode is deprecated",
                         DeprecationWarning, module='docutils.io')
 
-__version__ = '2.2.0'
-__released__ = '2.2.0'  # used when Sphinx builds its own docs
+__version__ = '2.3.0+'
+__released__ = '2.3.0'  # used when Sphinx builds its own docs
 
 #: Version info for better programmatic use.
 #:
@@ -43,7 +43,7 @@ __released__ = '2.2.0'  # used when Sphinx builds its own docs
 #:
 #: .. versionadded:: 1.2
 #:    Before version 1.2, check the string ``sphinx.__version__``.
-version_info = (2, 2, 0, 'final', 0)
+version_info = (2, 3, 0, 'beta', 0)
 
 package_dir = path.abspath(path.dirname(__file__))
 
