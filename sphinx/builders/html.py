0cfc00123b55e798e161f7d4064cbac3a8e61021
Nate Damon <ndamon@moog.com>
2019-10-24 10:36:54 -0400

    Reimplemented fix to not rely on error strings'

--- sphinx/builders/html.py
+++ sphinx/builders/html.py
@@ -1162,16 +1162,10 @@ def validate_html_extra_path(app: Sphinx, config: Config) -> None:
         if not path.exists(extra_path):
             logger.warning(__('html_extra_path entry %r does not exist'), entry)
             config.html_extra_path.remove(entry)
-        else:
-            try:
-                common_path = path.commonpath([app.outdir, extra_path])
-            except ValueError as e:  # different directories, can skip to next
-                if e.args[0] != "Paths don't have the same drive":
-                    raise e
-                continue
-            if common_path == app.outdir:
-                logger.warning(__('html_extra_path entry %r is placed inside outdir'), entry)
-                config.html_extra_path.remove(entry)
+        elif (path.splitdrive(app.outdir)[0] == path.splitdrive(extra_path) and
+              path.commonpath([app.outdir, extra_path]) == app.outdir):
+            logger.warning(__('html_extra_path entry %r is placed inside outdir'), entry)
+            config.html_extra_path.remove(entry)
 
 
 def validate_html_static_path(app: Sphinx, config: Config) -> None:
@@ -1181,16 +1175,10 @@ def validate_html_static_path(app: Sphinx, config: Config) -> None:
         if not path.exists(static_path):
             logger.warning(__('html_static_path entry %r does not exist'), entry)
             config.html_static_path.remove(entry)
-        else:
-            try:
-                common_path = path.commonpath([app.outdir, static_path])
-            except ValueError as e:  # different directories, can skip to next
-                if e.args[0] != "Paths don't have the same drive":
-                    raise e
-                continue
-            if common_path == app.outdir:
-                logger.warning(__('html_static_path entry %r is placed inside outdir'), entry)
-                config.html_static_path.remove(entry)
+        elif (path.splitdrive(app.outdir)[0] == path.splitdrive(static_path) and
+              path.commonpath([app.outdir, static_path]) == app.outdir):
+            logger.warning(__('html_static_path entry %r is placed inside outdir'), entry)
+            config.html_static_path.remove(entry)
 
 
 def validate_html_logo(app: Sphinx, config: Config) -> None:
