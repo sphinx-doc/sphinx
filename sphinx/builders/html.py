73b4404275c8599a656e6b3c9d08e8a9170eecbf
Nate Damon <ndamon@moog.com>
2019-10-24 11:06:19 -0400

    Corrected typo where drives were not compared, meaning the check always evaluates to false

--- sphinx/builders/html.py
+++ sphinx/builders/html.py
@@ -1162,7 +1162,7 @@ def validate_html_extra_path(app: Sphinx, config: Config) -> None:
         if not path.exists(extra_path):
             logger.warning(__('html_extra_path entry %r does not exist'), entry)
             config.html_extra_path.remove(entry)
-        elif (path.splitdrive(app.outdir)[0] == path.splitdrive(extra_path) and
+        elif (path.splitdrive(app.outdir)[0] == path.splitdrive(extra_path)[0] and
               path.commonpath([app.outdir, extra_path]) == app.outdir):
             logger.warning(__('html_extra_path entry %r is placed inside outdir'), entry)
             config.html_extra_path.remove(entry)
@@ -1175,7 +1175,7 @@ def validate_html_static_path(app: Sphinx, config: Config) -> None:
         if not path.exists(static_path):
             logger.warning(__('html_static_path entry %r does not exist'), entry)
             config.html_static_path.remove(entry)
-        elif (path.splitdrive(app.outdir)[0] == path.splitdrive(static_path) and
+        elif (path.splitdrive(app.outdir)[0] == path.splitdrive(static_path)[0] and
               path.commonpath([app.outdir, static_path]) == app.outdir):
             logger.warning(__('html_static_path entry %r is placed inside outdir'), entry)
             config.html_static_path.remove(entry)
