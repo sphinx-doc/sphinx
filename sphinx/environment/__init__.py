fa98dc467a2b01f7535f5084863088b37040038d
Takeshi KOMIYA <i.tkomiya@gmail.com>
2019-12-25 00:41:34 +0900

    Stop to use deprecated API: env.indexentries

--- sphinx/environment/__init__.py
+++ sphinx/environment/__init__.py
@@ -750,7 +750,7 @@ class BuildEnvironment:
     @property
     def indexentries(self) -> Dict[str, List[Tuple[str, str, str, str, str]]]:
         warnings.warn('env.indexentries() is deprecated. Please use IndexDomain instead.',
-                      RemovedInSphinx40Warning)
+                      RemovedInSphinx40Warning, stacklevel=2)
         from sphinx.domains.index import IndexDomain
         domain = cast(IndexDomain, self.get_domain('index'))
         return domain.entries
