5e4e44c19598aaeeda15422422cf5eec8136a9ea
Takeshi KOMIYA <i.tkomiya@gmail.com>
2020-02-01 13:38:26 +0900

    autodoc: Support type annotations for variables

--- sphinx/ext/autodoc/__init__.py
+++ sphinx/ext/autodoc/__init__.py
@@ -10,6 +10,7 @@
     :license: BSD, see LICENSE for details.
 """
 
+import importlib
 import re
 import warnings
 from types import ModuleType
@@ -33,6 +34,7 @@ from sphinx.util import logging
 from sphinx.util import rpartition
 from sphinx.util.docstrings import prepare_docstring
 from sphinx.util.inspect import getdoc, object_description, safe_getattr, stringify_signature
+from sphinx.util.typing import stringify as stringify_typehint
 
 if False:
     # For type annotation
@@ -1233,11 +1235,18 @@ class DataDocumenter(ModuleLevelDocumenter):
         sourcename = self.get_sourcename()
         if not self.options.annotation:
             try:
-                objrepr = object_description(self.object)
+                annotations = getattr(self.parent, '__annotations__', {})
+                if self.objpath[-1] in annotations:
+                    objrepr = stringify_typehint(annotations.get(self.objpath[-1]))
+                    self.add_line('   :type: ' + objrepr, sourcename)
             except ValueError:
                 pass
-            else:
-                self.add_line('   :annotation: = ' + objrepr, sourcename)
+
+            try:
+                objrepr = object_description(self.object)
+                self.add_line('   :value: ' + objrepr, sourcename)
+            except ValueError:
+                pass
         elif self.options.annotation is SUPPRESS:
             pass
         else:
@@ -1276,6 +1285,12 @@ class DataDeclarationDocumenter(DataDocumenter):
         """Never import anything."""
         # disguise as a data
         self.objtype = 'data'
+        try:
+            # import module to obtain type annotation
+            self.parent = importlib.import_module(self.modname)
+        except ImportError:
+            pass
+
         return True
 
     def add_content(self, more_content: Any, no_docstring: bool = False) -> None:
@@ -1405,11 +1420,18 @@ class AttributeDocumenter(DocstringStripSignatureMixin, ClassLevelDocumenter):  
         if not self.options.annotation:
             if not self._datadescriptor:
                 try:
-                    objrepr = object_description(self.object)
+                    annotations = getattr(self.parent, '__annotations__', {})
+                    if self.objpath[-1] in annotations:
+                        objrepr = stringify_typehint(annotations.get(self.objpath[-1]))
+                        self.add_line('   :type: ' + objrepr, sourcename)
                 except ValueError:
                     pass
-                else:
-                    self.add_line('   :annotation: = ' + objrepr, sourcename)
+
+                try:
+                    objrepr = object_description(self.object)
+                    self.add_line('   :value: ' + objrepr, sourcename)
+                except ValueError:
+                    pass
         elif self.options.annotation is SUPPRESS:
             pass
         else:
