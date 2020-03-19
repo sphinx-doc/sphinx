718b8f20557edf12c2b6ffe0558137c19030e320
Takeshi KOMIYA <i.tkomiya@gmail.com>
2020-03-19 23:10:15 +0900

    Fix #3842: singlehtml: Path to images broken when master doc is not in source root

--- sphinx/util/osutil.py
+++ sphinx/util/osutil.py
@@ -59,8 +59,8 @@ def relative_uri(base: str, to: str) -> str:
     """Return a relative URL from ``base`` to ``to``."""
     if to.startswith(SEP):
         return to
-    b2 = base.split(SEP)
-    t2 = to.split(SEP)
+    b2 = base.split('#')[0].split(SEP)
+    t2 = to.split('#')[0].split(SEP)
     # remove common segments (except the last segment)
     for x, y in zip(b2[:-1], t2[:-1]):
         if x != y:
