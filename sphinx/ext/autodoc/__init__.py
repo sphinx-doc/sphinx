dd70d9faac59c4367d3edc7a047c1951d54281a7
Takeshi KOMIYA <i.tkomiya@gmail.com>
2020-03-22 19:06:22 +0900

    Fix #7331: autodoc: a cython-method is not recognized as a method

--- sphinx/ext/autodoc/__init__.py
+++ sphinx/ext/autodoc/__init__.py
@@ -1431,7 +1431,8 @@ class MethodDocumenter(DocstringSignatureMixin, ClassLevelDocumenter):  # type: 
         if self.env.config.autodoc_typehints == 'none':
             kwargs.setdefault('show_annotation', False)
 
-        if inspect.isbuiltin(self.object) or inspect.ismethoddescriptor(self.object):
+        if ((inspect.isbuiltin(self.object) or inspect.ismethoddescriptor(self.object)) and
+                not inspect.is_cython_function_or_method(self.object)):
             # can never get arguments of a C function or method
             return None
         if inspect.isstaticmethod(self.object, cls=self.parent, name=self.object_name):
--- tests/roots/test-ext-autodoc/target/cython.pyx
+++ tests/roots/test-ext-autodoc/target/cython.pyx
@@ -2,3 +2,11 @@
 
 def foo(*args, **kwargs):
     """Docstring."""
+
+
+class Class:
+    """Docstring."""
+
+    def meth(self, name: str, age: int = 0) -> None:
+        """Docstring."""
+        pass
