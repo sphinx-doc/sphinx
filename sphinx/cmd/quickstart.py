97bc24500a7e62c34f92da2d53a36221fdd7c95d
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-11 00:22:36 +0200

    Fix py35 incompatibility with PosixPath

--- sphinx/cmd/quickstart.py
+++ sphinx/cmd/quickstart.py
@@ -358,7 +358,7 @@ def generate(d: Dict, overwrite: bool = True, silent: bool = False, templatedir:
     d.setdefault('extensions', [])
     d['copyright'] = time.strftime('%Y') + ', ' + d['author']
 
-    d["path"] = pathlib.Path(d['path']).resolve()
+    d["path"] = str(pathlib.Path(d['path']).resolve())
     ensuredir(d['path'])
 
     srcdir = path.join(d['path'], 'source') if d['sep'] else d['path']
