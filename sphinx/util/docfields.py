e79cd79cd2d1e46d57fd6b941bd6348eb53bdb17
Keewis <keewis@posteo.de>
2020-08-05 14:28:32 +0200

    revert the change to DocFieldTransformer

--- sphinx/util/docfields.py
+++ sphinx/util/docfields.py
@@ -322,8 +322,6 @@ class DocFieldTransformer:
             if typedesc.is_typed:
                 try:
                     argtype, argname = fieldarg.split(None, 1)
-                    if argtype.endswith(","):
-                        raise ValueError
                 except ValueError:
                     pass
                 else:
