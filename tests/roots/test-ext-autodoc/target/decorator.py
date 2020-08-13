221983a75a954d1e7b368d0cafea64c0076cb4e5
harupy <17039389+harupy@users.noreply.github.com>
2020-08-14 01:23:38 +0900

    simplify test

--- tests/roots/test-ext-autodoc/target/decorator.py
+++ tests/roots/test-ext-autodoc/target/decorator.py
@@ -31,52 +31,19 @@ class Bar:
         pass
 
 
-def deco_call(cls):
-    _original = cls.__call__
-
-    @wraps(_original)
-    def wrapper(self, *args, **kwargs):
-        return _original(self, *args, **kwargs)
-
-    cls.__call__ = wrapper
-    return cls
-
-
-@deco_call
 class BarCall:
+    @deco1
     def __call__(self, name=None, age=None):
         pass
 
 
-def deco_new(cls):
-    _original = cls.__new__
-
-    @wraps(_original)
-    def wrapper(cls, *args, **kwargs):
-        return _original(cls, *args, **kwargs)
-
-    cls.__new__ = wrapper
-    return cls
-
-
-@deco_new
 class BarNew:
+    @deco1
     def __new__(cls, name=None, age=None):
         pass
 
 
-def deco_init(cls):
-    _original = cls.__init__
-
-    @wraps(_original)
-    def wrapper(self, *args, **kwargs):
-        _original(self, *args, **kwargs)
-
-    cls.__init__ = wrapper
-    return cls
-
-
-@deco_init
 class BarInit:
+    @deco1
     def __init__(self, name=None, age=None):
         pass
