sphinx-doc/sphinx

commit 5f82825e273dfc5df3c4ef499876def8b024fa81

Customize jinja comment-tag

sphinx/util/template.py
@@ -84,6 +84,8 @@ class LaTeXRenderer(SphinxRenderer):
         self.env.variable_end_string = '%>'
         self.env.block_start_string = '<%'
         self.env.block_end_string = '%>'
+        self.env.comment_start_string = '<#'
+        self.env.comment_end_string = '<#'
 
 
 class ReSTRenderer(SphinxRenderer):

commit 6b87b8030c16e195ccd8b2246fe8d5f6d24e893b

Fix comment end string in LaTeXRenderer
Fix bug introduced by commit 5f82825e27 in `self.env.comment_end_string`

sphinx/util/template.py
@@ -85,7 +85,7 @@ class LaTeXRenderer(SphinxRenderer):
         self.env.block_start_string = '<%'
         self.env.block_end_string = '%>'
         self.env.comment_start_string = '<#'
-        self.env.comment_end_string = '<#'
+        self.env.comment_end_string = '#>'
 
 
 class ReSTRenderer(SphinxRenderer):