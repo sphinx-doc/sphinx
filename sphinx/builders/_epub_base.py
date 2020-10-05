3eecf133d8a8af2f22f6f122d3245a1e2db940eb
Thomas Grainger <tagrain@gmail.com>
2020-10-05 09:29:16 +0100

    Fix #8289: Allow to suppress "duplicated ToC entry found" warnings from epub builder

--- sphinx/builders/_epub_base.py
+++ sphinx/builders/_epub_base.py
@@ -208,7 +208,12 @@ class EpubBuilder(StandaloneHTMLBuilder):
         appeared = set()  # type: Set[str]
         for node in nodes:
             if node['refuri'] in appeared:
-                logger.warning(__('duplicated ToC entry found: %s'), node['refuri'])
+                logger.warning(
+                    __('duplicated ToC entry found: %s'),
+                    node['refuri'],
+                    type="epub",
+                    subtype="duplicated_toc_entry",
+                )
             else:
                 appeared.add(node['refuri'])
 
