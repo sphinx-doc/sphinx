35725f02d9fc42d711d8a377fd97e07aa8015958
oleg.hoefling <oleg.hoefling@gmail.com>
2020-10-08 17:08:18 +0200

    move registration of public markers to sphinx.testing.fixtures
    Signed-off-by: oleg.hoefling <oleg.hoefling@gmail.com>

--- sphinx/testing/fixtures.py
+++ sphinx/testing/fixtures.py
@@ -22,6 +22,21 @@ from sphinx.testing import util
 from sphinx.testing.util import SphinxTestApp, SphinxTestAppWrapperForSkipBuilding
 
 
+markers = [
+    (
+        'sphinx(builder, testroot=None, freshenv=False, confoverrides=None, tags=None,'
+        ' docutilsconf=None, parallel=0): arguments to initialize the sphinx test application.'
+    ),
+    'test_params(shared_result=...): test parameters.',
+]
+
+
+def pytest_configure(config):
+    # register custom markers
+    for marker in markers:
+        config.addinivalue_line('markers', marker)
+
+
 @pytest.fixture(scope='session')
 def rootdir() -> str:
     return None
