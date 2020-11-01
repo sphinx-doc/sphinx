95f313eab718ec55da327179a8b25a52e080c97b
Jon Dufresne <jon.dufresne@gmail.com>
2020-11-01 08:25:22 -0800

    Test and document support for Python 3.9 release

--- setup.py
+++ setup.py
@@ -202,6 +202,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Framework :: Setuptools Plugin',
