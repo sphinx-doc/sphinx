73db152cf65d090ef433db9acd49107ffd21aba3
Takeshi KOMIYA <i.tkomiya@gmail.com>
2021-01-20 23:21:11 +0900

    Close #5560: napoleon_use_param also affect "other parameters" section

--- sphinx/ext/napoleon/docstring.py
+++ sphinx/ext/napoleon/docstring.py
@@ -682,7 +682,13 @@ class GoogleDocstring:
         return self._parse_generic_section(_('Notes'), use_admonition)
 
     def _parse_other_parameters_section(self, section: str) -> List[str]:
-        return self._format_fields(_('Other Parameters'), self._consume_fields())
+        if self._config.napoleon_use_param:
+            # Allow to declare multiple parameters at once (ex: x, y: int)
+            fields = self._consume_fields(multiple=True)
+            return self._format_docutils_params(fields)
+        else:
+            fields = self._consume_fields()
+            return self._format_fields(_('Other Parameters'), fields)
 
     def _parse_parameters_section(self, section: str) -> List[str]:
         if self._config.napoleon_use_param:
