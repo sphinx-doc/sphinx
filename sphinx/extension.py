9def25b5dd82b64649331c166c1d59a287483d7f
Arthur Milchior <arthur@milchior.fr>
2021-11-26 02:35:42 +0100

    Ensure lines length is at most 95

--- sphinx/extension.py
+++ sphinx/extension.py
@@ -42,12 +42,13 @@ class Extension:
 
 
 def verify_needs_extensions(app: "Sphinx", config: Config) -> None:
-    """Check that extensions mentioned in :confval:`needs_extensions` satisfy the version requirement, and warn if an extension is not loaded.
+    """Check that extensions mentioned in :confval:`needs_extensions` satisfy the version
+    requirement, and warn if an extension is not loaded.
 
     Warns if a needed extension is not loaded.
-    :raises VersionRequirementError: if the version of an extension in :confval:`needs_extension` is
-                                     unknown or older than the required version.
-                                     """
+    :raises VersionRequirementError: if the version of an extension in
+    :confval:`needs_extension` is unknown or older than the required version.
+    """
     if config.needs_extensions is None:
         return
 
