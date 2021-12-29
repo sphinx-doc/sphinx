8a27fa0b9ac889deb43514c0f96a5fe43b87df26
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2021-12-29 02:43:14 +0000

    Update Scorer

--- sphinx/themes/basic/static/searchtools.js
+++ sphinx/themes/basic/static/searchtools.js
@@ -14,39 +14,40 @@
  */
 $u = _.noConflict();
 
-if (!Scorer) {
-  /**
-   * Simple result scoring code.
-   */
-  var Scorer = {
-    // Implement the following function to further tweak the score for each result
-    // The function takes a result array [filename, title, anchor, descr, score]
-    // and returns the new score.
-    /*
-    score: function(result) {
-      return result[4];
-    },
-    */
+/**
+ * Simple result scoring code.
+ */
+const Scorer = {
+  // Implement the following function to further tweak the score for each result
+  // The function takes a result array [filename, title, anchor, descr, score]
+  // and returns the new score.
+  /*
+  score: result => {
+    const [filename, title, anchor, descr, score] = result
+    return score
+  },
+  */
 
-    // query matches the full name of an object
-    objNameMatch: 11,
-    // or matches in the last dotted part of the object name
-    objPartialMatch: 6,
-    // Additive scores depending on the priority of the object
-    objPrio: {0:  15,   // used to be importantResults
-              1:  5,   // used to be objectResults
-              2: -5},  // used to be unimportantResults
-    //  Used when the priority is not in the mapping.
-    objPrioDefault: 0,
+  // query matches the full name of an object
+  objNameMatch: 11,
+  // or matches in the last dotted part of the object name
+  objPartialMatch: 6,
+  // Additive scores depending on the priority of the object
+  objPrio: {
+    0:  15,  // used to be importantResults
+    1:  5,   // used to be objectResults
+    2: -5    // used to be unimportantResults
+  },
+  //  Used when the priority is not in the mapping.
+  objPrioDefault: 0,
 
-    // query found in title
-    title: 15,
-    partialTitle: 7,
-    // query found in terms
-    term: 5,
-    partialTerm: 2
-  };
-}
+  // query found in title
+  title: 15,
+  partialTitle: 7,
+  // query found in terms
+  term: 5,
+  partialTerm: 2
+};
 
 if (!splitQuery) {
   function splitQuery(query) {
