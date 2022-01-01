75baf68645b0ddf441a681ca54dc3b901a04cd26
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2022-01-01 11:41:08 +0000

    Forgot this one

--- sphinx/themes/basic/static/searchtools.js
+++ sphinx/themes/basic/static/searchtools.js
@@ -13,37 +13,39 @@
 /**
  * Simple result scoring code.
  */
-const Scorer = {
-  // Implement the following function to further tweak the score for each result
-  // The function takes a result array [docname, title, anchor, descr, score, filename]
-  // and returns the new score.
-  /*
-  score: result => {
-    const [docname, title, anchor, descr, score, filename] = result
-    return score
-  },
-  */
+if (!Scorer) {
+  let Scorer = {
+    // Implement the following function to further tweak the score for each result
+    // The function takes a result array [docname, title, anchor, descr, score, filename]
+    // and returns the new score.
+    /*
+    score: result => {
+      const [docname, title, anchor, descr, score, filename] = result
+      return score
+    },
+    */
 
-  // query matches the full name of an object
-  objNameMatch: 11,
-  // or matches in the last dotted part of the object name
-  objPartialMatch: 6,
-  // Additive scores depending on the priority of the object
-  objPrio: {
-    0: 15, // used to be importantResults
-    1: 5, // used to be objectResults
-    2: -5, // used to be unimportantResults
-  },
-  //  Used when the priority is not in the mapping.
-  objPrioDefault: 0,
+    // query matches the full name of an object
+    objNameMatch: 11,
+    // or matches in the last dotted part of the object name
+    objPartialMatch: 6,
+    // Additive scores depending on the priority of the object
+    objPrio: {
+      0: 15, // used to be importantResults
+      1: 5, // used to be objectResults
+      2: -5, // used to be unimportantResults
+    },
+    //  Used when the priority is not in the mapping.
+    objPrioDefault: 0,
 
-  // query found in title
-  title: 15,
-  partialTitle: 7,
-  // query found in terms
-  term: 5,
-  partialTerm: 2,
-};
+    // query found in title
+    title: 15,
+    partialTitle: 7,
+    // query found in terms
+    term: 5,
+    partialTerm: 2,
+  };
+}
 
 const _removeChildren = (element) => {
   while (element.lastChild) element.removeChild(element.lastChild);
