f73d39b7d48f79003bc405a02cd0d313af015e8f
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-04-27 22:08:18 +0100

    Simplify the docstring in `PorterStemmer.stem()`

--- sphinx/util/stemmer/porter.py
+++ sphinx/util/stemmer/porter.py
@@ -377,13 +377,8 @@ class PorterStemmer:
             self.k = self.k - 1
 
     def stem(self, word: str) -> str:
-        """In stem(p,i,j), p is a char pointer, and the string to be stemmed
-        is from p[i] to p[j] inclusive. Typically i is zero and j is the
-        offset to the last character of a string, (p[j+1] == '\0'). The
-        stemmer adjusts the characters p[i] ... p[j] and returns the new
-        end-point of the string, k. Stemming never increases word length, so
-        i <= k <= j. To turn the stemmer into a module, declare 'stem' as
-        extern, and delete the remainder of this file.
+        """The string to be stemmed is ``word``.
+        The stemmer returns the stemmed string.
         """
         # copy the parameters into statics
         self.b = word
