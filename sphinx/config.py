20de387b9fa378b7784e86f9b38937428b38be91
Stephen Finucane <stephen@that.guru>
2022-05-31 12:39:19 +0100

    Correct typo
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- sphinx/config.py
+++ sphinx/config.py
@@ -170,7 +170,7 @@ class Config:
         #       (This conversion should not be removed before 2025-01-01).
         if namespace.get("language", ...) is None:
             logger.warning(__("Invalid configuration value found: 'language = None'. "
-                              "Update your configuration to a valid langauge code. "
+                              "Update your configuration to a valid language code. "
                               "Falling back to 'en' (English)."))
             namespace["language"] = "en"
 
