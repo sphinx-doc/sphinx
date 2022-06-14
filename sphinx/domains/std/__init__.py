e62ea7d18d37bae63fce31ac77ac96b5374f3ff9
Jakob Lykke Andersen <Jakob@caput.dk>
2022-06-14 21:00:12 +0200

    intersphinx, complete delegation for std

--- sphinx/domains/std/__init__.py
+++ sphinx/domains/std/__init__.py
@@ -27,6 +27,7 @@ if TYPE_CHECKING:
     from sphinx.application import Sphinx
     from sphinx.builders import Builder
     from sphinx.environment import BuildEnvironment
+    from sphinx.util.inventory import InventoryItemSet
     from sphinx.util.typing import ExtensionMetadata, OptionSpec, RoleFunction
 
 logger = logging.getLogger(__name__)
@@ -995,6 +996,35 @@ class StandardDomain(Domain):
                 results.append((role, make_refnode(builder, fromdocname, docname,
                                                    labelid, contnode)))
         return results
+
+    def _intersphinx_adjust_object_types(self, objtypes: list[str]) -> None:
+        # we adjust the object types for backwards compatibility
+        if 'cmdoption' in objtypes:
+            # until Sphinx-1.6, cmdoptions are stored as std:option
+            objtypes.append('option')
+
+    def _intersphinx_resolve_xref_lookup(self, store: dict[str, dict[str, InventoryItemSet]],
+                                         target: str, objtypes: list[str]
+                                         ) -> InventoryItemSet | None:
+        # Semi-haxy overwriting of the private method in Domain, as we only need to do
+        # case-insensitive lookup for label term
+        for objtype in objtypes:
+            if objtype not in store:
+                continue
+
+            if target in store[objtype]:
+                # Case-sensitive match, use it
+                return store[objtype][target]
+            elif objtype in ('label', 'term'):
+                # Some types require case-insensitive matches:
+                # * 'term': https://github.com/sphinx-doc/sphinx/issues/9291
+                # * 'label': https://github.com/sphinx-doc/sphinx/issues/12008
+                target_lower = target.lower()
+                insensitive_matches = list(filter(lambda k: k.lower() == target_lower,
+                                                  store[objtype].keys()))
+                if insensitive_matches:
+                    return store[objtype][insensitive_matches[0]]
+        return None
 
     def get_objects(self) -> Iterator[tuple[str, str, str, str, str, int]]:
         # handle the special 'doc' reference here
