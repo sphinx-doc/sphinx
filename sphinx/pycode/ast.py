2b9461a753c2965f2d852c45c3e475710b296404
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-06-15 22:15:01 +0100

    Typo

--- sphinx/pycode/ast.py
+++ sphinx/pycode/ast.py
@@ -141,7 +141,7 @@ class _UnparseVisitor(ast.NodeVisitor):
         return "%s.%s" % (self.visit(node.value), node.attr)
 
     def visit_BinOp(self, node: ast.BinOp) -> str:
-        # Special case ``**`` to now have surrounding spaces.
+        # Special case ``**`` to not have surrounding spaces.
         if isinstance(node.op, ast.Pow):
             return "".join(map(self.visit, (node.left, node.op, node.right)))
         return " ".join(self.visit(e) for e in [node.left, node.op, node.right])
