498214df6d5da698629794fe8a95f6e502d270b8
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-09-23 18:04:54 +0100

    Add test

--- tests/test_build_html.py
+++ tests/test_build_html.py
@@ -131,6 +131,16 @@ def test_html4_output(app, status, warning):
     app.build()
 
 
+def test_html4_deprecation(make_app, tempdir):
+    (tempdir / 'conf.py').write_text('', encoding='utf-8')
+    app = make_app(
+        buildername='html',
+        srcdir=tempdir,
+        confoverrides={'html4_writer': True},
+    )
+    assert 'HTML 4 output is deprecated and will be removed' in app._warning.getvalue()
+
+
 @pytest.mark.parametrize("fname,expect", flat_dict({
     'images.html': [
         (".//img[@src='_images/img.png']", ''),
