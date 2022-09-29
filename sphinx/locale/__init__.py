a8851b3f7776ee8853fb28a7bba99bcfb5c9f041
James Addison <jay@jp-hosting.net>
2022-09-29 01:06:16 +0100

    Remove _TranslationProxy locale resource lazy-loading mechanism
    Attempts to address issues related to the proxy that appear to be related to the point-in-time at which resource catalogs become available during documentation builds.
    
    Ref: https://github.com/sphinx-doc/sphinx/issues/9778

--- sphinx/locale/__init__.py
+++ sphinx/locale/__init__.py
@@ -6,90 +6,6 @@ from os import path
 from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 
-class _TranslationProxy:
-    """
-    Class for proxy strings from gettext translations. This is a helper for the
-    lazy_* functions from this module.
-
-    The proxy implementation attempts to be as complete as possible, so that
-    the lazy objects should mostly work as expected, for example for sorting.
-    """
-    __slots__ = ('_func', '_args')
-
-    def __new__(cls, func: Callable, *args: str) -> '_TranslationProxy':
-        if not args:
-            # not called with "function" and "arguments", but a plain string
-            return str(func)  # type: ignore[return-value]
-        return object.__new__(cls)
-
-    def __getnewargs__(self) -> Tuple[str]:
-        return (self._func,) + self._args  # type: ignore
-
-    def __init__(self, func: Callable, *args: str) -> None:
-        self._func = func
-        self._args = args
-
-    def __str__(self) -> str:
-        return str(self._func(*self._args))
-
-    def __dir__(self) -> List[str]:
-        return dir(str)
-
-    def __getattr__(self, name: str) -> Any:
-        return getattr(self.__str__(), name)
-
-    def __getstate__(self) -> Tuple[Callable, Tuple[str, ...]]:
-        return self._func, self._args
-
-    def __setstate__(self, tup: Tuple[Callable, Tuple[str]]) -> None:
-        self._func, self._args = tup
-
-    def __copy__(self) -> '_TranslationProxy':
-        return _TranslationProxy(self._func, *self._args)
-
-    def __repr__(self) -> str:
-        try:
-            return 'i' + repr(str(self.__str__()))
-        except Exception:
-            return f'<{self.__class__.__name__} broken>'
-
-    def __add__(self, other: str) -> str:
-        return self.__str__() + other
-
-    def __radd__(self, other: str) -> str:
-        return other + self.__str__()
-
-    def __mod__(self, other: str) -> str:
-        return self.__str__() % other
-
-    def __rmod__(self, other: str) -> str:
-        return other % self.__str__()
-
-    def __mul__(self, other: Any) -> str:
-        return self.__str__() * other
-
-    def __rmul__(self, other: Any) -> str:
-        return other * self.__str__()
-
-    def __hash__(self):
-        return hash(self.__str__())
-
-    def __eq__(self, other):
-        return self.__str__() == other
-
-    def __lt__(self, string):
-        return self.__str__() < string
-
-    def __contains__(self, char):
-        return char in self.__str__()
-
-    def __len__(self):
-        return len(self.__str__())
-
-    def __getitem__(self, index):
-        return self.__str__()[index]
-
-
 translators: Dict[Tuple[str, str], NullTranslations] = {}
 
 
@@ -181,18 +97,6 @@ def get_translator(catalog: str = 'sphinx', namespace: str = 'general') -> NullT
     return translators.get((namespace, catalog), NullTranslations())
 
 
-def is_translator_registered(catalog: str = 'sphinx', namespace: str = 'general') -> bool:
-    return (namespace, catalog) in translators
-
-
-def _lazy_translate(catalog: str, namespace: str, message: str) -> str:
-    """Used instead of _ when creating TranslationProxy, because _ is
-    not bound yet at that time.
-    """
-    translator = get_translator(catalog, namespace)
-    return translator.gettext(message)
-
-
 def get_translation(catalog: str, namespace: str = 'general') -> Callable[[str], str]:
     """Get a translation function based on the *catalog* and *namespace*.
 
@@ -219,15 +123,11 @@ def get_translation(catalog: str, namespace: str = 'general') -> Callable[[str],
     .. versionadded:: 1.8
     """
     def gettext(message: str, *args: Any) -> str:
-        if not is_translator_registered(catalog, namespace):
-            # not initialized yet
-            return _TranslationProxy(_lazy_translate, catalog, namespace, message)  # type: ignore  # NOQA
-        else:
-            translator = get_translator(catalog, namespace)
-            if len(args) <= 1:
-                return translator.gettext(message)
-            else:  # support pluralization
-                return translator.ngettext(message, args[0], args[1])
+        translator = get_translator(catalog, namespace)
+        if len(args) <= 1:
+            return translator.gettext(message)
+        else:  # support pluralization
+            return translator.ngettext(message, args[0], args[1])
 
     return gettext
 
