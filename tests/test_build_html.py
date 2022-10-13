927ef07c5556516891bbc5f5d569ca0f53d09ec8
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-10-13 15:52:58 +0100

    Move to new test root

--- tests/test_build_html.py
+++ tests/test_build_html.py
@@ -1397,11 +1397,11 @@ def test_html_remote_images(app, status, warning):
     assert not (app.outdir / 'python-logo.png').exists()
 
 
-@pytest.mark.sphinx('html')
+@pytest.mark.sphinx('html', testroot='image-escape')
 def test_html_encoded_image(app, status, warning):
     app.builder.build_all()
 
-    result = (app.outdir / 'images.html').read_text()
+    result = (app.outdir / 'index.html').read_text()
     assert ('<img alt="_images/img_%231.png" src="_images/img_%231.png" />' in result)
     assert (app.outdir / '_images/img_#1.png').exists()
 
