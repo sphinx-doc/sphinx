2902c6c55be460aae10b08504ed2fde4f3795f64
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2023-03-17 16:41:22 +0000

    Deprecate legacy ``intersphinx_mapping`` format (#11247)
    This format was made obsolete in Sphinx 1.0, but never formally deprecated.

--- sphinx/ext/intersphinx.py
+++ sphinx/ext/intersphinx.py
@@ -617,7 +617,15 @@ def normalize_intersphinx_mapping(app: Sphinx, config: Config) -> None:
                     continue
             else:
                 # old format, no name
+                # xref RemovedInSphinx80Warning
                 name, uri, inv = None, key, value
+                logger.warning(
+                    "The pre-Sphinx 1.0 'intersphinx_mapping' format is "
+                    "deprecated and will be removed in Sphinx 8. Update to the "
+                    "current format as described in the documentation. "
+                    f"Hint: \"intersphinx_mapping = {{'<name>': {(uri, inv)!r}}}\"."
+                    "https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_mapping",  # NoQA: E501
+                )
 
             if not isinstance(inv, tuple):
                 config.intersphinx_mapping[key] = (name, (uri, (inv,)))
