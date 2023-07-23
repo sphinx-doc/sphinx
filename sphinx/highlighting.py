5949be853b5537af14e4e21715318e1665538e37
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-07-23 18:32:30 +0100

    style

--- sphinx/highlighting.py
+++ sphinx/highlighting.py
@@ -153,15 +153,8 @@ class PygmentsBridge:
 
         return lexer
 
-    def highlight_block(
-        self,
-        source: str,
-        lang: str,
-        opts: dict | None = None,
-        force: bool = False,
-        location: Any = None,
-        **kwargs: Any,
-    ) -> str:
+    def highlight_block(self, source: str, lang: str, opts: dict | None = None,
+                        force: bool = False, location: Any = None, **kwargs: Any) -> str:
         if not isinstance(source, str):
             source = source.decode()
 
