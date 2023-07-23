927062ee4dc73a2ba2fa40ada43bdb9312e645d9
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-07-23 22:11:53 +0100

    Override UA and TLS on ``Session.request()``

--- sphinx/util/requests.py
+++ sphinx/util/requests.py
@@ -3,8 +3,7 @@
 from __future__ import annotations
 
 import warnings
-from contextlib import contextmanager
-from typing import Any, Iterator
+from typing import Any
 from urllib.parse import urlsplit
 
 import requests
@@ -14,15 +13,6 @@ import sphinx
 
 _USER_AGENT = (f'Mozilla/5.0 (X11; Linux x86_64; rv:100.0) Gecko/20100101 Firefox/100.0 '
                f'Sphinx/{sphinx.__version__}')
-
-
-@contextmanager
-def ignore_insecure_warning(verify: bool) -> Iterator[None]:
-    with warnings.catch_warnings():
-        if not verify:
-            # ignore InsecureRequestWarning if verify=False
-            warnings.filterwarnings("ignore", category=InsecureRequestWarning)
-        yield
 
 
 def _get_tls_cacert(url: str, certs: str | dict[str, str] | None) -> str | bool:
@@ -40,7 +30,7 @@ def _get_tls_cacert(url: str, certs: str | dict[str, str] | None) -> str | bool:
 
 
 def get(url: str, **kwargs: Any) -> requests.Response:
-    """Sends a HEAD request like requests.head().
+    """Sends a GET request like requests.get().
 
     This sets up User-Agent header and TLS verification automatically."""
     with _Session() as session:
@@ -56,15 +46,13 @@ def head(url: str, **kwargs: Any) -> requests.Response:
 
 
 class _Session(requests.Session):
-
-    def get(  # type: ignore[override]
-        self,
-        url: str,
+    def request(  # type: ignore[override]
+        self, method: str, url: str,
         _user_agent: str = '',
         _tls_info: tuple[bool, str | dict[str, str] | None] = (),  # type: ignore[assignment]
         **kwargs: Any
     ) -> requests.Response:
-        """Sends a GET request like requests.get().
+        """Sends a request with an HTTP verb and url.
 
         This sets up User-Agent header and TLS verification automatically."""
         headers = kwargs.setdefault('headers', {})
@@ -76,27 +64,10 @@ class _Session(requests.Session):
         else:
             verify = kwargs.get('verify', True)
 
-        with ignore_insecure_warning(verify):
-            return super().get(url, **kwargs)
+        if verify:
+            return super().request(method, url, **kwargs)
 
-    def head(  # type: ignore[override]
-        self,
-        url: str,
-        _user_agent: str = '',
-        _tls_info: tuple[bool, str | dict[str, str] | None] = (),  # type: ignore[assignment]
-        **kwargs: Any
-    ) -> requests.Response:
-        """Sends a HEAD request like requests.head().
-
-        This sets up User-Agent header and TLS verification automatically."""
-        headers = kwargs.setdefault('headers', {})
-        headers.setdefault('User-Agent', _user_agent or _USER_AGENT)
-        if _tls_info:
-            tls_verify, tls_cacerts = _tls_info
-            verify = bool(kwargs.get('verify', tls_verify))
-            kwargs.setdefault('verify', verify and _get_tls_cacert(url, tls_cacerts))
-        else:
-            verify = kwargs.get('verify', True)
-
-        with ignore_insecure_warning(verify):
-            return super().head(url, **kwargs)
+        with warnings.catch_warnings():
+            # ignore InsecureRequestWarning if verify=False
+            warnings.filterwarnings("ignore", category=InsecureRequestWarning)
+            return super().request(method, url, **kwargs)
