b464611fea19b998ccd9233153e168c50dc89cf5
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-08-10 23:46:51 +0100

    condense

--- sphinx/environment/__init__.py
+++ sphinx/environment/__init__.py
@@ -503,10 +503,9 @@ class BuildEnvironment:
                         deppath = path.join(self.srcdir, dep)
                         if not path.isfile(deppath):
                             logger.debug(
-                                '[build target] changed %r '
-                                'missing dependency %r',
-                                docname,
-                                deppath)
+                                '[build target] changed %r missing dependency %r',
+                                docname, deppath,
+                            )
                             changed.add(docname)
                             break
                         depmtime = _last_modified_time(deppath)
@@ -518,12 +517,8 @@ class BuildEnvironment:
                                 depmtime / 1_000_000, tz=timezone.utc,
                             )
                             logger.debug(
-                                '[build target] outdated %r '
-                                'from dependency %r: %s -> %s',
-                                docname,
-                                deppath,
-                                mtime_dt,
-                                depmtime_dt,
+                                '[build target] outdated %r from dependency %r: %s -> %s',
+                                docname, deppath, mtime_dt, depmtime_dt,
                             )
                             changed.add(docname)
                             break
