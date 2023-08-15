9a0def925f68d99151dc290d95d471b48d9092c2
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2023-08-15 13:37:36 +0200

    include MathJax if one of the included documents requires it

--- sphinx/domains/math.py
+++ sphinx/domains/math.py
@@ -134,10 +134,16 @@ class MathDomain(Domain):
         return []
 
     def has_equations(self, docname: str | None = None) -> bool:
-        if docname:
-            return self.data['has_equations'].get(docname, False)
-        else:
-            return any(self.data['has_equations'].values())
+        markers = self.data['has_equations']
+
+        if not docname:
+            return any(markers.values())
+
+        if markers.get(docname, False):
+            return True
+
+        if included := self.env.toctree_includes.get(docname, ()):
+            return any(self.has_equations(otherdoc) for otherdoc in included)
 
 
 def setup(app: Sphinx) -> dict[str, Any]:
