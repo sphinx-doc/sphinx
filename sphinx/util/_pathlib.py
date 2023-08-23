955b553c690b51c010d7136c5bb16ae7e5639855
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-08-23 16:57:02 +0100

    replace

--- sphinx/util/_pathlib.py
+++ sphinx/util/_pathlib.py
@@ -8,12 +8,20 @@ from pathlib import Path, PosixPath, WindowsPath
 
 from sphinx.deprecation import RemovedInSphinx80Warning
 
-_STR_METHODS = frozenset(dir(''))
+_STR_METHODS = frozenset(str.__dict__)
 _PATH_NAME = Path().__class__.__name__
 
 
 if sys.platform == 'win32':
     class _StrPath(WindowsPath):
+        def replace(self, old, new, count=-1, /):
+            # replace exists in both Path and str;
+            # in Path it makes filesystem changes, so we use the safer str version
+            warnings.warn('Sphinx 8 will drop support for representing paths as strings. '
+                          'Use "pathlib.Path" or "os.fspath" instead.',
+                          RemovedInSphinx80Warning, stacklevel=2)
+            return str(self).replace(old, new, count)
+
         def __getattr__(self, item):
             if item in _STR_METHODS:
                 warnings.warn('Sphinx 8 will drop support for representing paths as strings. '
@@ -24,6 +32,12 @@ if sys.platform == 'win32':
             raise AttributeError(msg)
 else:
     class _StrPath(PosixPath):
+        def replace(self, old, new, count=-1, /):
+            warnings.warn('Sphinx 8 will drop support for representing paths as strings. '
+                          'Use "pathlib.Path" or "os.fspath" instead.',
+                          RemovedInSphinx80Warning, stacklevel=2)
+            return str(self).replace(old, new, count)
+
         def __getattr__(self, item):
             if item in _STR_METHODS:
                 warnings.warn('Sphinx 8 will drop support for representing paths as strings. '
