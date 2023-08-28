0550be78eaa0b60c25e52d0e11952e3d65dbf67f
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2023-08-28 05:21:18 +0100

    Avoid indirection

--- sphinx/directives/other.py
+++ sphinx/directives/other.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import re
+from os.path import abspath
 from typing import TYPE_CHECKING, Any, cast
 
 from docutils import nodes
@@ -18,7 +19,7 @@ from sphinx.util import docname_join, logging, url_re
 from sphinx.util.docutils import SphinxDirective
 from sphinx.util.matching import Matcher, patfilter
 from sphinx.util.nodes import explicit_title_re
-from sphinx.util.osutil import abspath, os_path
+from sphinx.util.osutil import os_path
 
 if TYPE_CHECKING:
     from docutils.nodes import Element, Node
@@ -386,7 +387,7 @@ class Include(BaseInclude, SphinxDirective):
             text = "\n".join(include_lines[:-2])
 
             # The docname to pass into the source-read event
-            docname = self.env.path2doc(os_path(abspath(source)))
+            docname = self.env.path2doc(abspath(os_path(source)))
             # Emit the "source-read" event
             arg = [text]
             self.env.app.events.emit("source-read", docname, arg)
