267df5f3cb303b25035a6accad9285a553e31287
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-01-18 19:49:15 +0000

    Reflect C++ changes in the C domain

--- sphinx/domains/c.py
+++ sphinx/domains/c.py
@@ -149,6 +149,8 @@ class ASTIdentifier(ASTBaseBase):
         assert len(identifier) != 0
         self.identifier = identifier
 
+    # ASTBaseBase already implements this method,
+    # but specialising it here improves performance
     def __eq__(self, other: Any) -> bool:
         return type(other) is ASTIdentifier and self.identifier == other.identifier
 
@@ -1448,6 +1450,10 @@ class ASTDeclaration(ASTBaseBase):
         # set by CObject._add_enumerator_to_parent
         self.enumeratorScopedSymbol: Symbol | None = None
 
+        # the cache assumes that by the time get_newest_id is called, no
+        # further changes will be made to this object
+        self._newest_id_cache: str | None = None
+
     def clone(self) -> ASTDeclaration:
         return ASTDeclaration(self.objectType, self.directiveType,
                               self.declaration.clone(), self.semicolon)
@@ -1474,7 +1480,9 @@ class ASTDeclaration(ASTBaseBase):
             return id_
 
     def get_newest_id(self) -> str:
-        return self.get_id(_max_id, True)
+        if self._newest_id_cache is None:
+            self._newest_id_cache = self.get_id(_max_id, True)
+        return self._newest_id_cache
 
     def _stringify(self, transform: StringifyTransform) -> str:
         res = transform(self.declaration)
