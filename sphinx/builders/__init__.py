7e176119ffeee6dba56e860707f13a312c2e9e7d
Lynn-Hu-D <lingyuhu0106@gmail.com>
2024-02-25 13:12:12 -0800

    add specific error messages when root_doc not in include_patterns

--- sphinx/builders/__init__.py
+++ sphinx/builders/__init__.py
@@ -419,10 +419,15 @@ class Builder:
         else:
             self._read_serial(docnames)
 
-        if self.config.root_doc not in self.env.all_docs:
-            raise SphinxError('root file %s not found' %
-                              self.env.doc2path(self.config.root_doc))
-
+        # check if the root_doc exists
+        if self.config.root_doc not in self.env.all_docs: 
+            if  "**" not in self.config.include_patterns:
+                raise SphinxError('customized include_patterns is set, but root file %s not in the include_patterns' %
+                        self.env.doc2path(self.config.root_doc))
+            else: 
+                raise SphinxError('root file %s not found' %
+                                self.env.doc2path(self.config.root_doc))
+        
         for retval in self.events.emit('env-updated', self.env):
             if retval is not None:
                 docnames.extend(retval)
