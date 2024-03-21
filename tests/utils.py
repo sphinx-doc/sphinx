07514f2ae012ad75a2d48b2f259057b74b73b978
James Addison <jay@jp-hosting.net>
2024-03-21 00:11:05 +0000

    [linting] Remove tests/utils.py from ruff exclusion list.

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ("http_server",)
+__all__ = ('http_server',)
 
 from contextlib import contextmanager
 from http.server import ThreadingHTTPServer
@@ -20,7 +20,7 @@ if TYPE_CHECKING:
 # $ openssl req -new -x509 -days 3650 -nodes -out cert.pem \
 #     -keyout cert.pem -addext "subjectAltName = DNS:localhost"
 TESTS_ROOT: Final[Path] = Path(__file__).parent
-CERT_FILE: Final[str] = str(TESTS_ROOT / "certs" / "cert.pem")
+CERT_FILE: Final[str] = str(TESTS_ROOT / 'certs' / 'cert.pem')
 
 # File lock for tests
 LOCK_PATH: Final[str] = str(TESTS_ROOT / 'test-server.lock')
@@ -29,7 +29,7 @@ LOCK_PATH: Final[str] = str(TESTS_ROOT / 'test-server.lock')
 class HttpServerThread(Thread):
     def __init__(self, handler: type[BaseRequestHandler]) -> None:
         super().__init__(daemon=True)
-        self.server = ThreadingHTTPServer(("localhost", 7777), handler)
+        self.server = ThreadingHTTPServer(('localhost', 7777), handler)
 
     def run(self) -> None:
         self.server.serve_forever(poll_interval=0.001)
@@ -49,7 +49,9 @@ class HttpsServerThread(HttpServerThread):
 
 
 @contextmanager
-def http_server(handler: type[BaseRequestHandler], *, tls_enabled: bool = False) -> Iterator[HttpServerThread]:
+def http_server(
+    handler: type[BaseRequestHandler], *, tls_enabled: bool = False
+) -> Iterator[HttpServerThread]:
     server_cls = HttpsServerThread if tls_enabled else HttpServerThread
     with filelock.FileLock(LOCK_PATH):
         server = server_cls(handler)
