967687ebd93361a875d6172a5ae69355929bb7a5
James Addison <jay@jp-hosting.net>
2024-03-21 00:09:52 +0000

    [tests]: utils: simplify HttpServerThread signatures in preparation for reformatting.

--- tests/utils.py
+++ tests/utils.py
@@ -14,7 +14,7 @@ import filelock
 if TYPE_CHECKING:
     from collections.abc import Iterator
     from socketserver import BaseRequestHandler
-    from typing import Any, Final
+    from typing import Final
 
 # Generated with:
 # $ openssl req -new -x509 -days 3650 -nodes -out cert.pem \
@@ -27,8 +27,8 @@ LOCK_PATH: Final[str] = str(TESTS_ROOT / 'test-server.lock')
 
 
 class HttpServerThread(Thread):
-    def __init__(self, handler: type[BaseRequestHandler], /, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, handler: type[BaseRequestHandler]) -> None:
+        super().__init__(daemon=True)
         self.server = ThreadingHTTPServer(("localhost", 7777), handler)
 
     def run(self) -> None:
@@ -41,10 +41,8 @@ class HttpServerThread(Thread):
 
 
 class HttpsServerThread(HttpServerThread):
-    def __init__(
-        self, handler: type[BaseRequestHandler], /, *args: Any, **kwargs: Any,
-    ) -> None:
-        super().__init__(handler, *args, **kwargs)
+    def __init__(self, handler: type[BaseRequestHandler]) -> None:
+        super().__init__(handler)
         sslcontext = SSLContext(PROTOCOL_TLS_SERVER)
         sslcontext.load_cert_chain(CERT_FILE)
         self.server.socket = sslcontext.wrap_socket(self.server.socket, server_side=True)
@@ -54,7 +52,7 @@ class HttpsServerThread(HttpServerThread):
 def http_server(handler: type[BaseRequestHandler], *, tls_enabled: bool = False) -> Iterator[HttpServerThread]:
     server_cls = HttpsServerThread if tls_enabled else HttpServerThread
     with filelock.FileLock(LOCK_PATH):
-        server = server_cls(handler, daemon=True)
+        server = server_cls(handler)
         server.start()
         try:
             yield server
