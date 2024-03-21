7f50a5b63482a0c1c512b5eb7e6f1256f12ed16e
Chris Sewell <chrisj_sewell@hotmail.com>
2024-03-21 11:11:29 +0100

    fix lint

--- sphinx/util/typing.py
+++ sphinx/util/typing.py
@@ -101,11 +101,11 @@ class ExtensionMetadata(TypedDict, total=False):
     env_version: int
     """An integer that identifies the version of env data added by the extension."""
     parallel_read_safe: bool
-    """Indicate whether parallel reading of source files is supported 
+    """Indicate whether parallel reading of source files is supported
     by the extension.
     """
     parallel_write_safe: bool
-    """Indicate whether parallel writing of output files is supported 
+    """Indicate whether parallel writing of output files is supported
     by the extension (default: ``True``).
     """
 
