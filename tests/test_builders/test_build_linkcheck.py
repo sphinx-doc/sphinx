c2ed645042c7ca146936bc49f24d9c4ddb2800a5
James Addison <jay@jp-hosting.net>
2024-03-22 10:20:34 +0000

    [refactor] tests: relocate linkcheck-related settings in test arrangement-phase to Sphinx fixture 'confoverrides'.

--- tests/test_builders/test_build_linkcheck.py
+++ tests/test_builders/test_build_linkcheck.py
@@ -916,8 +916,8 @@ def test_limit_rate_default_sleep(app: Sphinx) -> None:
     assert next_check == 60.0
 
 
+@pytest.mark.sphinx(confoverrides={'linkcheck_rate_limit_timeout': 0.0})
 def test_limit_rate_user_max_delay(app: Sphinx) -> None:
-    app.config.linkcheck_rate_limit_timeout = 0.0  # type: ignore[attr-defined]
     worker = HyperlinkAvailabilityCheckWorker(app.config, Queue(), Queue(), {})
     next_check = worker.limit_rate(FakeResponse.url, FakeResponse.headers.get("Retry-After"))
     assert next_check is None
@@ -931,8 +931,8 @@ def test_limit_rate_doubles_previous_wait_time(app: Sphinx) -> None:
     assert next_check == 120.0
 
 
+@pytest.mark.sphinx(confoverrides={'linkcheck_rate_limit_timeout': 90.0})
 def test_limit_rate_clips_wait_time_to_max_time(app: Sphinx) -> None:
-    app.config.linkcheck_rate_limit_timeout = 90.0  # type: ignore[attr-defined]
     rate_limits = {"localhost": RateLimit(60.0, 0.0)}
     worker = HyperlinkAvailabilityCheckWorker(app.config, Queue(), Queue(), rate_limits)
     with mock.patch('time.time', return_value=0.0):
@@ -940,8 +940,8 @@ def test_limit_rate_clips_wait_time_to_max_time(app: Sphinx) -> None:
     assert next_check == 90.0
 
 
+@pytest.mark.sphinx(confoverrides={'linkcheck_rate_limit_timeout': 90.0})
 def test_limit_rate_bails_out_after_waiting_max_time(app: Sphinx) -> None:
-    app.config.linkcheck_rate_limit_timeout = 90.0  # type: ignore[attr-defined]
     rate_limits = {"localhost": RateLimit(90.0, 0.0)}
     worker = HyperlinkAvailabilityCheckWorker(app.config, Queue(), Queue(), rate_limits)
     next_check = worker.limit_rate(FakeResponse.url, FakeResponse.headers.get("Retry-After"))
