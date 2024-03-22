fb66b899cf043e7b0d4b4226f09c567e00f90767
James Addison <jay@jp-hosting.net>
2024-03-22 23:08:01 +0000

    PEP526: remove docstrings from module variables.
    The __doc__ attribute -- docstrings -- is not specified in PEP526
    for module-level variables.
    
    Quoting https://peps.python.org/pep-0257/#what-is-a-docstring below:
    
    > A docstring is a string literal that occurs as the first statement
    > in a module, function, class, or method definition.  Such a
    > docstring becomes the __doc__ special attribute of that object.
    
    Relates-to commit 34add11b5d1f50849ad7c751e8a6a0317bec9dbd.

--- tests/test_extensions/ext_napoleon_pep526_data_google.py
+++ tests/test_extensions/ext_napoleon_pep526_data_google.py
@@ -1,8 +1,5 @@
 """Test module for napoleon PEP 526 compatibility with google style"""
 
-module_level_var: int = 99
-"""This is an example module level variable"""
-
 
 class PEP526GoogleClass:
     """Sample class with PEP 526 annotations and google docstring.
