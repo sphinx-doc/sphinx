64ecd41b3d4a4e3f683245488b29f8ed37e1f490
James Addison <jay@jp-hosting.net>
2024-03-22 14:27:35 +0000

    [linting] Reformat using 'ruff format'.
    (cherry picked from commit 9bed49541ec660420273b2e02724b15d09c9fd07)

--- utils/generate_js_fixtures.py
+++ utils/generate_js_fixtures.py
@@ -4,28 +4,28 @@ import subprocess
 from pathlib import Path
 
 SPHINX_ROOT = Path(__file__).resolve().parents[1]
-TEST_JS_FIXTURES = SPHINX_ROOT / "tests" / "js" / "fixtures"
-TEST_JS_ROOTS = SPHINX_ROOT / "tests" / "js" / "roots"
+TEST_JS_FIXTURES = SPHINX_ROOT / 'tests' / 'js' / 'fixtures'
+TEST_JS_ROOTS = SPHINX_ROOT / 'tests' / 'js' / 'roots'
 
 
 def build(srcdir: Path) -> None:
-    cmd = ("sphinx-build", "-E", "-q", "-b", "html", f"{srcdir}", f"{srcdir}/build")
+    cmd = ('sphinx-build', '-E', '-q', '-b', 'html', f'{srcdir}', f'{srcdir}/build')
     subprocess.run(cmd, check=True, capture_output=True)
 
 
 def beautify(filename: Path) -> None:
-    cmd = ("js-beautify", "-r", filename)
+    cmd = ('js-beautify', '-r', filename)
     subprocess.run(cmd, check=True, capture_output=True)
 
 
 for directory in TEST_JS_ROOTS.iterdir():
-    searchindex = directory / "build" / "searchindex.js"
-    destination = TEST_JS_FIXTURES / directory.name / "searchindex.js"
+    searchindex = directory / 'build' / 'searchindex.js'
+    destination = TEST_JS_FIXTURES / directory.name / 'searchindex.js'
 
-    print(f"Building {directory} ... ", end="")
+    print(f'Building {directory} ... ', end='')
     build(directory)
-    print("done")
+    print('done')
 
-    print(f"Moving {searchindex} to {destination} ... ", end="")
+    print(f'Moving {searchindex} to {destination} ... ', end='')
     searchindex.replace(destination)
-    print("done")
+    print('done')
