a5209e95b7c5733a7f2bd9476c59016d33b1384f
James Addison <jay@jp-hosting.net>
2024-03-23 00:20:05 +0000

    Revert "PEP526: remove docstrings from module variables."
    This reverts commit fb66b899cf043e7b0d4b4226f09c567e00f90767.

--- tests/test_extensions/ext_napoleon_pep526_data_numpy.py
+++ tests/test_extensions/ext_napoleon_pep526_data_numpy.py
@@ -1,5 +1,8 @@
 """Test module for napoleon PEP 526 compatibility with numpy style"""
 
+module_level_var: int = 99
+"""This is an example module level variable"""
+
 
 class PEP526NumpyClass:
     """
