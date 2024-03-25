c411cf1a51375a0862122db91023aa4a0a79c424
James Addison <jay@jp-hosting.net>
2024-03-25 18:46:03 +0000

    [linkcheck] tests: anchor-quoting safety-belt coverage.

--- tests/test_builders/test_build_linkcheck.py
+++ tests/test_builders/test_build_linkcheck.py
@@ -287,7 +287,7 @@ def test_anchors_ignored_for_url(app):
 
     attrs = ('filename', 'lineno', 'status', 'code', 'uri', 'info')
     data = [json.loads(x) for x in content.splitlines()]
-    assert len(data) == 7
+    assert len(data) == 8
     assert all(all(attr in row for attr in attrs) for row in data)
 
     # rows may be unsorted due to network latency or
@@ -296,6 +296,7 @@ def test_anchors_ignored_for_url(app):
 
     assert rows['http://localhost:7777/valid']['status'] == 'working'
     assert rows['http://localhost:7777/valid#valid-anchor']['status'] == 'working'
+    assert rows['http://localhost:7777/valid#py:module::urllib.parse']['status'] == 'broken'
     assert rows['http://localhost:7777/valid#invalid-anchor'] == {
         'status': 'broken',
         'info': "Anchor 'invalid-anchor' not found",
