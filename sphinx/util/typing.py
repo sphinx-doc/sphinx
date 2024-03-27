ae4145e92f1fe4ea8f76f74066db9cba1fd71263
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-03-27 09:31:49 +0100

    revert some format

--- sphinx/util/typing.py
+++ sphinx/util/typing.py
@@ -72,7 +72,7 @@ PathMatcher = Callable[[str], bool]
 
 # common role functions
 RoleFunction = Callable[[str, str, str, int, Inliner, dict[str, Any], Sequence[str]],
-                         tuple[list[nodes.Node], list[nodes.system_message]]]
+                        tuple[list[nodes.Node], list[nodes.system_message]]]
 
 # A option spec for directive
 OptionSpec = dict[str, Callable[[str], Any]]
