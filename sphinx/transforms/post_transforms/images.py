c1ca06a95177e3fe8613d0a287a147aa3716bbc9
James Addison <jay@jp-hosting.net>
2024-04-10 12:20:27 +0100

    Revert "Remote image post-transform: add a character-translation for port number segments."
    This reverts commit 9ca2670e72d59e504a21330db94ce1f8ba07b67b.

--- sphinx/transforms/post_transforms/images.py
+++ sphinx/transforms/post_transforms/images.py
@@ -64,8 +64,7 @@ class ImageDownloader(BaseImageConverter):
                 basename = sha1(filename.encode(), usedforsecurity=False).hexdigest() + ext
             basename = CRITICAL_PATH_CHAR_RE.sub("_", basename)
 
-            dirname = node['uri'].replace('://', '/').translate({ord(":"): "/",
-                                                                 ord("?"): "/",
+            dirname = node['uri'].replace('://', '/').translate({ord("?"): "/",
                                                                  ord("&"): "/"})
             if len(dirname) > MAX_FILENAME_LEN:
                 dirname = sha1(dirname.encode(), usedforsecurity=False).hexdigest()
