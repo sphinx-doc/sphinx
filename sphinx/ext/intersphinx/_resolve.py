ab781829ef6d7fc7b7e66ee37da55b2d0f1698be
James Addison <jay@jp-hosting.net>
2024-04-29 14:53:37 +0100

    Intersphinx lookups: log inventory name at both load-time and ambiguous-resolution-time.

--- sphinx/ext/intersphinx/_resolve.py
+++ sphinx/ext/intersphinx/_resolve.py
@@ -81,8 +81,9 @@ def _resolve_reference_in_domain_by_target(
             insensitive_matches = list(filter(lambda k: k.lower() == target_lower,
                                               inventory[objtype].keys()))
             if len(insensitive_matches) > 1:
-                LOGGER.warning(__("multiple matches found for %s:%s"),
-                               objtype, target,
+                inv_descriptor = inv_name or 'main_inventory'
+                LOGGER.warning(__("inventory '%s': multiple matches found for %s:%s"),
+                               inv_descriptor, objtype, target,
                                type='intersphinx',  subtype='external', location=node)
             if insensitive_matches:
                 data = inventory[objtype][insensitive_matches[0]]
