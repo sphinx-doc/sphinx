d84d4e65dbdf56132ada5f9c96a8c5c64f69228e
Nicolas Peugnet <nicolas@club1.fr>
2024-05-19 21:23:12 +0200

    Fix <unknown> source file in translated warnings

--- sphinx/parsers.py
+++ sphinx/parsers.py
@@ -72,7 +72,7 @@ class RSTParser(docutils.parsers.rst.Parser, Parser):
             inputstring = inputstring[:-1]
 
         reporter = document.reporter
-        reporter.get_source_and_line = lambda x: (document.source, x)  # type: ignore[attr-defined]
+        reporter.get_source_and_line = lambda x: (document['source'], x)  # type: ignore[attr-defined]
         language = languages.get_language(document.settings.language_code, reporter)
         if self.inliner is None:
             inliner = states.Inliner()
@@ -86,7 +86,7 @@ class RSTParser(docutils.parsers.rst.Parser, Parser):
         )
         textnodes, messages = inliner.parse(inputstring, lineno, memo, document)
         p = nodes.paragraph(inputstring, '', *textnodes)
-        p.source, p.line = document.source, lineno
+        p.source, p.line = document['source'], lineno
         document += [p, *messages]
 
     def parse(self, inputstring: str | StringList, document: nodes.document) -> None:
