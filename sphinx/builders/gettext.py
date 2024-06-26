292941bf66e2f092ce28e739d7020a39f9b5f812
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-06-26 20:41:45 +0100

    Refactor ``Only.run()`` to operate on the input text

--- sphinx/builders/gettext.py
+++ sphinx/builders/gettext.py
@@ -123,7 +123,7 @@ class I18nTags(Tags):
     always returns True value even if no tags are defined.
     """
 
-    def eval_condition(self, condition: Any) -> bool:
+    def eval_condition(self, condition: str) -> bool:
         return True
 
 
@@ -140,7 +140,7 @@ class I18nBuilder(Builder):
         super().init()
         self.env.set_versioning_method(self.versioning_method,
                                        self.env.config.gettext_uuid)
-        self.tags = I18nTags()
+        self.tags = self.app.tags = I18nTags()
         self.catalogs: defaultdict[str, Catalog] = defaultdict(Catalog)
 
     def get_target_uri(self, docname: str, typ: str | None = None) -> str:
