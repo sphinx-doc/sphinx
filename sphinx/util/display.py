c07e2efac4589959073ff0af91e5a7480b01fc98
Daniel Eades <danieleades@hotmail.com>
2024-07-14 20:31:17 +0100

    fixup

--- sphinx/util/display.py
+++ sphinx/util/display.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import functools
-from types import TracebackType
 from typing import TYPE_CHECKING, Any, Callable, ParamSpec, TypeVar
 
 from sphinx.locale import __
