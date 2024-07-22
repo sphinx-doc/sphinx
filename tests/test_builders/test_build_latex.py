e174df2762569b13601336069c848ec1a96e6682
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2024-07-22 13:32:44 +0100

    Rename ``normalize_intersphinx_mapping`` to ``validate_intersphinx_mapping`` (#12643)

--- tests/test_builders/test_build_latex.py
+++ tests/test_builders/test_build_latex.py
@@ -13,7 +13,7 @@ import pytest
 from sphinx.builders.latex import default_latex_documents
 from sphinx.config import Config
 from sphinx.errors import SphinxError
-from sphinx.ext.intersphinx import load_mappings, normalize_intersphinx_mapping
+from sphinx.ext.intersphinx import load_mappings, validate_intersphinx_mapping
 from sphinx.ext.intersphinx import setup as intersphinx_setup
 from sphinx.util.osutil import ensuredir
 from sphinx.writers.latex import LaTeXTranslator
@@ -133,7 +133,7 @@ def test_build_latex_doc(app, engine, docclass, python_maximum_signature_line_le
         app.config.latex_table_style = ['booktabs']
     elif engine == 'lualatex':
         app.config.latex_table_style = ['colorrows']
-    normalize_intersphinx_mapping(app, app.config)
+    validate_intersphinx_mapping(app, app.config)
     load_mappings(app)
     app.builder.init()
     LaTeXTranslator.ignore_missing_images = True
