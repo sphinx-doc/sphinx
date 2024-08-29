sphinx-doc/sphinx

commit e439c6f33f8d8e7bc06b4f6f25dadfda74869ee0

Ensure that old-style object description options are respected (#12620)

sphinx/domains/javascript.py
@@ -309,7 +309,7 @@ class JSModule(SphinxDirective):
     def run(self) -> list[Node]:
         mod_name = self.arguments[0].strip()
         self.env.ref_context['js:module'] = mod_name
-        no_index = 'no-index' in self.options or 'noindex' in self.options
+        no_index = 'no-index' in self.options
 
         content_nodes = self.parse_content_to_nodes(allow_section_headings=True)
 

sphinx/domains/python/__init__.py
@@ -452,7 +452,7 @@ class PyModule(SphinxDirective):
         domain = cast(PythonDomain, self.env.get_domain('py'))
 
         modname = self.arguments[0].strip()
-        no_index = 'no-index' in self.options or 'noindex' in self.options
+        no_index = 'no-index' in self.options
         self.env.ref_context['py:module'] = modname
 
         content_nodes = self.parse_content_to_nodes(allow_section_headings=True)

sphinx/directives/__init__.py
@@ -220,21 +220,26 @@ class ObjectDescription(SphinxDirective, Generic[ObjDescT]):
         node['domain'] = self.domain
         # 'desctype' is a backwards compatible attribute
         node['objtype'] = node['desctype'] = self.objtype
+
+        # Copy old option names to new ones
+        # xref RemovedInSphinx90Warning
+        # deprecate noindex, noindexentry, and nocontentsentry in Sphinx 9.0
+        if 'no-index' not in self.options and 'noindex' in self.options:
+            self.options['no-index'] = self.options['noindex']
+        if 'no-index-entry' not in self.options and 'noindexentry' in self.options:
+            self.options['no-index-entry'] = self.options['noindexentry']
+        if 'no-contents-entry' not in self.options and 'nocontentsentry' in self.options:
+            self.options['no-contents-entry'] = self.options['nocontentsentry']
+
         node['no-index'] = node['noindex'] = no_index = (
             'no-index' in self.options
-            # xref RemovedInSphinx90Warning
-            # deprecate noindex in Sphinx 9.0
-            or 'noindex' in self.options)
+        )
         node['no-index-entry'] = node['noindexentry'] = (
             'no-index-entry' in self.options
-            # xref RemovedInSphinx90Warning
-            # deprecate noindexentry in Sphinx 9.0
-            or 'noindexentry' in self.options)
+        )
         node['no-contents-entry'] = node['nocontentsentry'] = (
             'no-contents-entry' in self.options
-            # xref RemovedInSphinx90Warning
-            # deprecate nocontentsentry in Sphinx 9.0
-            or 'nocontentsentry' in self.options)
+        )
         node['no-typesetting'] = ('no-typesetting' in self.options)
         if self.domain:
             node['classes'].append(self.domain)

commit e3754ba60ce4b93ecf1919093da3803ab106d070

Restore 'no-index' support for JS, Python domains
Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: e439c6f33f (Ensure that old-style object description options are respected (#12620))
Closes: #12843

sphinx/domains/javascript.py
@@ -309,6 +309,13 @@ class JSModule(SphinxDirective):
     def run(self) -> list[Node]:
         mod_name = self.arguments[0].strip()
         self.env.ref_context['js:module'] = mod_name
+
+        # Copy old option names to new ones
+        # xref RemovedInSphinx90Warning
+        # # deprecate noindex in Sphinx 9.0
+        if 'no-index' not in self.options and 'noindex' in self.options:
+            self.options['no-index'] = self.options['noindex']
+
         no_index = 'no-index' in self.options
 
         content_nodes = self.parse_content_to_nodes(allow_section_headings=True)

sphinx/domains/python/__init__.py
@@ -451,6 +451,12 @@ class PyModule(SphinxDirective):
     def run(self) -> list[Node]:
         domain = cast(PythonDomain, self.env.get_domain('py'))
 
+        # Copy old option names to new ones
+        # xref RemovedInSphinx90Warning
+        # # deprecate noindex in Sphinx 9.0
+        if 'no-index' not in self.options and 'noindex' in self.options:
+            self.options['no-index'] = self.options['noindex']
+
         modname = self.arguments[0].strip()
         no_index = 'no-index' in self.options
         self.env.ref_context['py:module'] = modname