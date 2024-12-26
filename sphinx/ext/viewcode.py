67c732d8fd4b96f927f8ca7532f36a069c5a83f4
ProGamerGov <ProGamerGov@users.noreply.github.com>
2024-12-26 11:57:01 -0700

    Fix linting errors

--- sphinx/ext/viewcode.py
+++ sphinx/ext/viewcode.py
@@ -46,7 +46,6 @@ class viewcode_anchor(Element):
     For not supported builders, they will be removed.
     """
 
-
 def _get_full_modname(modname: str, attribute: str) -> str | None:
     try:
         try:
@@ -55,49 +54,48 @@ def _get_full_modname(modname: str, attribute: str) -> str | None:
                 # then it's better to return it directly
                 return None
             module = import_module(modname)
-    
+
             # Allow an attribute to have multiple parts and incidentally allow
             # repeated .s in the attribute.
             value = module
-            for attr in attribute.split('.'):
+            for attr in attribute.split("."):
                 if attr:
                     value = getattr(value, attr)
-    
-            return getattr(value, '__module__', None)
+
+            return getattr(value, "__module__", None)
         except ModuleNotFoundError:
             # Attempt to find full path of module
-            module_path = modname.split('.')
+            module_path = modname.split(".")
             actual_path = __import__(module_path[0], globals(), locals(), [], 0)
             if len(module_path) > 1:
                 for mod in module_path[1:]:
                     actual_path = getattr(actual_path, mod)
-    
+
             # Extract path from module name
             actual_path_str = str(actual_path).split("'")[1]
-    
+
             # Load module with exact path
             module = import_module(actual_path_str)
             value = module
-            for attr in attribute.split('.'):
+            for attr in attribute.split("."):
                 if attr:
                     value = getattr(value, attr)
-    
-            return getattr(value, '__module__', None)
-            raise 
-    
+
+            return getattr(value, "__module__", None)
+
     except AttributeError:
-            # sphinx.ext.viewcode can't follow class instance attribute
-            # then AttributeError logging output only debug mode.
-            logger.debug("Didn't find %s in %s", attribute, modname)
-            return None
+        # sphinx.ext.viewcode can't follow class instance attribute
+        # then AttributeError logging output only debug mode.
+        logger.debug("Didn't find %s in %s", attribute, modname)
+        return None
     except Exception as e:
-            # sphinx.ext.viewcode follow python domain directives.
-            # because of that, if there are no real modules exists that specified
-            # by py:function or other directives, viewcode emits a lot of warnings.
-            # It should be displayed only verbose mode.
-            logger.verbose(traceback.format_exc().rstrip())
-            logger.verbose('viewcode can\'t import %s, failed with error "%s"', modname, e)
-            return None
+        # sphinx.ext.viewcode follow python domain directives.
+        # because of that, if there are no real modules exists that specified
+        # by py:function or other directives, viewcode emits a lot of warnings.
+        # It should be displayed only verbose mode.
+        logger.verbose(traceback.format_exc().rstrip())
+        logger.verbose('viewcode can\'t import %s, failed with error "%s"', modname, e)
+        return None
 
 
 def is_supported_builder(builder: Builder) -> bool:
