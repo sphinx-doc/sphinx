64937535dca170adb1e2b9be48d27b3e3b2dbab6
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-01-02 21:11:36 +0000

    Bump Ruff to 0.8.5

--- tests/test_util/test_util_inspect.py
+++ tests/test_util/test_util_inspect.py
@@ -813,10 +813,10 @@ def test_isgenericalias():
     T = List[int]  # NoQA: UP006
     S = list[Union[str, None]]  # NoQA: UP006, UP007
 
-    C = Callable[[int], None]  # a generic alias not having a doccomment
+    C = Callable[[int], None]  # NoQA: UP006  # a generic alias not having a doccomment
 
     assert inspect.isgenericalias(C)
-    assert inspect.isgenericalias(Callable)
+    assert inspect.isgenericalias(Callable)  # NoQA: UP006
     assert inspect.isgenericalias(T)
     assert inspect.isgenericalias(List)  # NoQA: UP006
     assert inspect.isgenericalias(S)
@@ -843,7 +843,7 @@ def test_getdoc_inherited_classmethod():
         @classmethod
         def meth(self):
             """
-            docstring
+            Docstring
                 indented text
             """
 
@@ -861,7 +861,7 @@ def test_getdoc_inherited_decorated_method():
     class Foo:
         def meth(self):
             """
-            docstring
+            Docstring
                 indented text
             """
 
