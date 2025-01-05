bcbccd1cf45834da3584da38b02d70e16de2e8f0
ProGamerGov <ProGamerGov@users.noreply.github.com>
2025-01-04 19:37:33 -0700

    fix remaining lint errors

--- tests/roots/test-ext-viewcode-find-package/conf.py
+++ tests/roots/test-ext-viewcode-find-package/conf.py
@@ -18,7 +18,7 @@ if 'test_linkcode' in tags:
             return 'http://foobar/source/%s.py' % fn
         elif domain == 'js':
             return 'http://foobar/js/' + info['fullname']
-        elif domain in ('c', 'cpp'):
+        elif domain in {'c', 'cpp'}:
             return 'http://foobar/%s/%s' % (domain, ''.join(info['names']))
         else:
             raise AssertionError
