d0f4f55ffc4acee571672be5688007081e38d4d7
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-01-21 19:24:10 +0000

    Test path too

--- tests/test_extensions/test_ext_autodoc_importer.py
+++ tests/test_extensions/test_ext_autodoc_importer.py
@@ -10,9 +10,16 @@ if TYPE_CHECKING:
 
 
 def test_import_native_module_stubs(rootdir: Path) -> None:
+    fish_licence_root = rootdir / 'test-ext-apidoc-duplicates'
+
     sys_path = list(sys.path)
-    sys.path.insert(0, str(rootdir / 'test-ext-apidoc-duplicates'))
+    sys.path.insert(0, str(fish_licence_root))
     halibut = import_module('fish_licence.halibut')
+    sys.path[:] = sys_path
+
     assert halibut.__file__.endswith('halibut.pyi')
     assert halibut.__spec__.origin.endswith('halibut.pyi')
-    sys.path[:] = sys_path
+
+    halibut_path = Path(halibut.__file__).resolve()
+    assert halibut_path.is_file()
+    assert halibut_path == fish_licence_root / 'fish_licence' / 'halibut.pyi'
