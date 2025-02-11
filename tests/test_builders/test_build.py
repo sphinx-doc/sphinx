bb5716b54a9b7dc026593b1410510d429891ccca
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-02-11 01:34:12 +0000

    Split out ``test_build_all``

--- tests/test_builders/test_build.py
+++ tests/test_builders/test_build.py
@@ -3,9 +3,7 @@
 from __future__ import annotations
 
 import os
-import shutil
 from contextlib import contextmanager
-from unittest import mock
 
 import pytest
 from docutils import nodes
@@ -14,59 +12,6 @@ from sphinx.cmd.build import build_main
 from sphinx.errors import SphinxError
 
 from tests.utils import TESTS_ROOT
-
-
-def request_session_head(url, **kwargs):
-    response = mock.Mock()
-    response.status_code = 200
-    response.url = url
-    return response
-
-
-@pytest.fixture
-def nonascii_srcdir(request, rootdir, sphinx_test_tempdir):
-    # Build in a non-ASCII source dir
-    test_name = '\u65e5\u672c\u8a9e'
-    basedir = sphinx_test_tempdir / request.node.originalname
-    srcdir = basedir / test_name
-    if not srcdir.exists():
-        shutil.copytree(rootdir / 'test-root', srcdir)
-
-    # add a doc with a non-ASCII file name to the source dir
-    (srcdir / (test_name + '.txt')).write_text(
-        """
-nonascii file name page
-=======================
-""",
-        encoding='utf8',
-    )
-
-    root_doc = srcdir / 'index.txt'
-    root_doc.write_text(
-        root_doc.read_text(encoding='utf8')
-        + f"""
-.. toctree::
-
-{test_name}/{test_name}
-""",
-        encoding='utf8',
-    )
-    return srcdir
-
-
-# note: this test skips building docs for some builders because they have independent testcase.
-#       (html, changes, epub, latex, texinfo and manpage)
-@pytest.mark.parametrize(
-    'buildername',
-    ['dirhtml', 'singlehtml', 'text', 'xml', 'pseudoxml', 'linkcheck'],
-)
-@mock.patch(
-    'sphinx.builders.linkcheck.requests.head',
-    side_effect=request_session_head,
-)
-def test_build_all(requests_head, make_app, nonascii_srcdir, buildername):
-    app = make_app(buildername, srcdir=nonascii_srcdir)
-    app.build()
 
 
 def test_root_doc_not_found(tmp_path, make_app):
