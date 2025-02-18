e960c3e81a394478b2f61b8dd2ac63627f297bb8
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-02-18 18:54:27 +0000

    simpler test

--- tests/test_extensions/test_ext_autodoc.py
+++ tests/test_extensions/test_ext_autodoc.py
@@ -6,6 +6,7 @@ source file translated by test_build.
 
 from __future__ import annotations
 
+from functools import singledispatchmethod
 from typing import TYPE_CHECKING
 from unittest.mock import Mock
 
@@ -15,11 +16,43 @@ from sphinx.ext.autodoc import Options
 
 # NEVER import these objects from sphinx.ext.autodoc directly
 from sphinx.ext.autodoc.directive import DocumenterBridge
+from sphinx.util.inspect import is_singledispatch_method
 
 from tests.test_extensions.autodoc_util import do_autodoc
 
 if TYPE_CHECKING:
     from sphinx.environment import BuildEnvironment
+
+
+class Foo:
+    """docstring"""
+
+    @singledispatchmethod
+    def meth(self, arg, kwarg=None):
+        """A method for general use."""
+        pass
+
+    @meth.register(int)
+    @meth.register(float)
+    def _meth_int(self, arg, kwarg=None):
+        """A method for int."""
+        pass
+
+    @meth.register(str)
+    def _meth_str(self, arg, kwarg=None):
+        """A method for str."""
+        pass
+
+    @meth.register
+    def _meth_dict(self, arg: dict, kwarg=None):
+        """A method for dict."""
+        # This function tests for specifying type through annotations
+        pass
+
+
+def test_is_singledispatch_method():
+    obj = Foo.__dict__['meth']
+    assert isinstance(obj, singledispatchmethod)
 
 
 def make_directive_bridge(env: BuildEnvironment) -> DocumenterBridge:
