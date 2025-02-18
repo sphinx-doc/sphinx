179e24252b1a6c9ad6b9415402eda9a9f09d2b67
Chris Sewell <chrisj_sewell@hotmail.com>
2025-02-18 01:30:30 +0100

    Add the ``sphinx.ext.apidoc`` extension (#13333)
    A common use-case is that users simply want to point Sphinx
    towards a Python module, and have it generate documentation
    automatically.
    
    This is not possible currently, without a "pre-build" step
    of running the ``sphinx-autogen`` CLI.
    
    This PR adds ``sphinx.ext.apidoc`` as a first-party extension,
    to incorporate the source file generation into the Sphinx build.
    
    Co-authored-by: Adam Turner <9087854+aa-turner@users.noreply.github.com>

--- sphinx/ext/apidoc/_shared.py
+++ sphinx/ext/apidoc/_shared.py
@@ -9,7 +9,9 @@ from sphinx.util import logging
 if TYPE_CHECKING:
     from collections.abc import Sequence, Set
     from pathlib import Path
-    from typing import Final
+    from typing import Final, Self
+
+    from sphinx.config import Config
 
 LOGGER: Final[logging.SphinxLoggerAdapter] = logging.getLogger('sphinx.ext.apidoc')
 
@@ -35,15 +37,12 @@ def _remove_old_files(
 class ApidocOptions:
     """Options for apidoc."""
 
-    module_path: Path
     dest_dir: Path
+    module_path: Path
 
     exclude_pattern: Sequence[str] = ()
-    quiet: bool = False
     max_depth: int = 4
-    force: bool = False
     follow_links: bool = False
-    dry_run: bool = False
     separate_modules: bool = False
     include_private: bool = False
     toc_file: str = 'modules'
@@ -53,7 +52,11 @@ class ApidocOptions:
     automodule_options: Set[str] = dataclasses.field(default_factory=set)
     suffix: str = 'rst'
 
-    remove_old: bool = False
+    remove_old: bool = True
+
+    quiet: bool = False
+    dry_run: bool = False
+    force: bool = True
 
     # --full only
     full: bool = False
@@ -64,3 +67,33 @@ class ApidocOptions:
     release: str | None = None
     extensions: Sequence[str] | None = None
     template_dir: str | None = None
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+class ApidocDefaults:
+    """Default values for apidoc options."""
+
+    exclude_patterns: list[str]
+    automodule_options: frozenset[str]
+    max_depth: int
+    follow_links: bool
+    separate_modules: bool
+    include_private: bool
+    no_headings: bool
+    module_first: bool
+    implicit_namespaces: bool
+
+    @classmethod
+    def from_config(cls, config: Config, /) -> Self:
+        """Collect the default values for apidoc options."""
+        return cls(
+            exclude_patterns=config.apidoc_exclude_patterns,
+            automodule_options=frozenset(config.apidoc_automodule_options),
+            max_depth=config.apidoc_max_depth,
+            follow_links=config.apidoc_follow_links,
+            separate_modules=config.apidoc_separate_modules,
+            include_private=config.apidoc_include_private,
+            no_headings=config.apidoc_no_headings,
+            module_first=config.apidoc_module_first,
+            implicit_namespaces=config.apidoc_implicit_namespaces,
+        )
