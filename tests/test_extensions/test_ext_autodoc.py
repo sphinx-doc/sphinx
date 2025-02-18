fba8636729aad55bf6b8aa7dbc88122e921c0009
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-02-18 19:19:38 +0000

    Test signatures

--- tests/test_extensions/test_ext_autodoc.py
+++ tests/test_extensions/test_ext_autodoc.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 import functools
 import sys
 import types
+from inspect import Parameter, Signature
 
 import pytest
+
+from sphinx.util import inspect
 
 from tests.test_extensions.autodoc_util import do_autodoc
 
@@ -35,16 +38,48 @@ class Foo:
         pass
 
 
+def annotate_to_first_argument(func, typ):
+    sig = inspect.signature(func)
+    if len(sig.parameters) == 0:
+        return None
+
+    def dummy():
+        pass
+
+    params = list(sig.parameters.values())
+    if params[0].annotation is Parameter.empty:
+        params[0] = params[0].replace(annotation=typ)
+        try:
+            dummy.__signature__ = sig.replace(parameters=params)  # type: ignore[attr-defined]
+            return dummy
+        except (AttributeError, TypeError):
+            # failed to update signature (ex. built-in or extension types)
+            return None
+
+    return func
+
+
 def test_is_singledispatch_method():
     print(sys.version_info)
     assert isinstance(Foo.__dict__['meth'], functools.singledispatchmethod)
-    if sys.version_info >= (3, 14, 0, 'alpha', 6):
+    if sys.version_info >= (3, 14, 0, 'alpha', 5):
         assert isinstance(Foo.meth, functools._singledispatchmethod_get)
     else:
         assert isinstance(Foo.meth, types.FunctionType)
+
     meth = Foo.__dict__['meth']
     assert len(meth.dispatcher.registry) == 5
     assert list(meth.dispatcher.registry) == [object, float, int, str, dict]
+
+    dispatch_methods = [annotate_to_first_argument(func, typ) for typ, func in meth.dispatcher.registry.items()]
+    dispatch_method_signatures = [str(Signature.from_callable(m)) for m in dispatch_methods]
+    assert dispatch_method_signatures == [
+        '(self: object, arg, kwarg=None)',
+        '(self: float, arg, kwarg=None)',
+        '(self: int, arg, kwarg=None)',
+        '(self: str, arg, kwarg=None)',
+        '(self: dict, arg: dict, kwarg=None)',
+    ]
 
 
 @pytest.mark.sphinx('html', testroot='ext-autodoc')
