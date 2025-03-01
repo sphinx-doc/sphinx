287ee204e4a381707760351cdae54af36114ee59
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-03-01 22:00:50 +0000

    Define jieba fallback functions

--- sphinx/search/zh.py
+++ sphinx/search/zh.py
@@ -4,19 +4,33 @@ from __future__ import annotations
 
 import re
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import snowballstemmer
 
 from sphinx.search import SearchLanguage
 
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
 try:
     import jieba  # type: ignore[import-not-found]
-
-    JIEBA = True
-    JIEBA_DEFAULT_DICT = Path(jieba.__file__).parent / jieba.DEFAULT_DICT_NAME
+    from jieba import cut_for_search
+    from jieba import load_userdict as jieba_load_userdict
 except ImportError:
-    JIEBA = False
-    JIEBA_DEFAULT_DICT = Path()
+    JIEBA_DEFAULT_DICT = ''
+
+    def jieba_load_userdict(f: str) -> None:
+        pass
+
+    def cut_for_search(sentence: str, HMM: bool = True) -> Iterator[str]:
+        yield from ()
+
+else:
+    JIEBA_DEFAULT_DICT = (
+        Path(jieba.__file__, '..', jieba.DEFAULT_DICT_NAME).resolve().as_posix()
+    )
+    del jieba
 
 english_stopwords = {
     'a', 'and', 'are', 'as', 'at',
@@ -231,18 +245,14 @@ class SearchChinese(SearchLanguage):
         self.latin_terms: set[str] = set()
 
     def init(self, options: dict[str, str]) -> None:
-        if JIEBA:
-            dict_path = options.get('dict', JIEBA_DEFAULT_DICT)
-            if dict_path and Path(dict_path).is_file():
-                jieba.load_userdict(str(dict_path))
+        dict_path = options.get('dict', JIEBA_DEFAULT_DICT)
+        if dict_path and Path(dict_path).is_file():
+            jieba_load_userdict(str(dict_path))
 
         self.stemmer = snowballstemmer.stemmer('english')
 
     def split(self, input: str) -> list[str]:
-        if JIEBA:
-            chinese: list[str] = list(jieba.cut_for_search(input))
-        else:
-            chinese = []
+        chinese: list[str] = list(cut_for_search(input))
 
         latin1 = [term.strip() for term in self.latin1_letters.findall(input)]
         self.latin_terms.update(latin1)
