a5a73b6da213d3a410282e17044e65eca3fb0787
user27182 <89109579+user27182@users.noreply.github.com>
2025-04-05 18:30:44 -0600

    Add options and update docs

--- doc/usage/extensions/duration.rst
+++ doc/usage/extensions/duration.rst
@@ -7,32 +7,68 @@
 .. versionadded:: 2.4
 
 This extension measures durations of Sphinx processing and show its
-result at end of the build.  It is useful for inspecting what document
-is slowly built.
-
-Configuration
-=============
-
-.. confval:: duration_options
-   :type: :code-py:`dict[str, int]`
-   :default: :code-py:`{'n_durations': 5}`
-
-   A dictionary for configuring the behavior of the duration extension.
-
-   The supported options are:
-
-   * ``'n_durations'``: Maximum number of durations to show in the build summary.
-     The durations are sorted in order from longest to shortest. Only the ``5`` slowest
-     durations are shown by default. Set this to ``-1`` to show all durations.
-
-   .. versionadded:: 8.3
-
-Example configuration in `conf.py` to show all durations in the build summary
+result at end of the build. It is useful for inspecting what document
+is slowly built. Enable this extension by adding it to ``conf.py``:
 
 .. code-block:: python
 
    extensions = ['sphinx.ext.duration']
-   duration_options = {'n_durations': -1}
+
+Optionally, configure the extension by adding ``duration_options`` as a config value.
+In ``conf.py``:
+
+.. code-block:: python
+
+   extensions = ['sphinx.ext.duration']
+
+   # disable printing totals and writing durations
+   # and only show the 10 slowest times
+   duration_options = {
+       'print_total': False,
+       'print_slowest' : True,
+       'n_slowest': 10,
+       'write_durations' : False,
+   }
 
    def setup(app):
        app.add_config_value('duration_options', duration_options, 'env')
+
+
+Configuration
+=============
+
+Configure this extension using a ``duration_options`` dictionary.
+
+.. confval:: print_slowest
+   :type: :code-py:`bool`
+   :default: :code-py:`True
+
+   Show the slowest durations in the build summary.
+
+.. confval:: n_slowest
+   :type: :code-py:`int`
+   :default: :code-py:`5`
+
+   Maximum number of slowest durations to show in the build summary.
+   The durations are sorted in order from slow to fast. Only the ``5`` slowest
+   durations are shown by default. Set this to ``-1`` to show all durations.
+
+   .. versionadded:: 8.3
+
+.. confval:: print_total
+   :type: :code-py:`bool`
+   :default: :code-py:`True`
+
+   Show the total reading duration in the build summary.
+
+   .. versionadded:: 8.3
+
+.. confval:: write_durations
+   :type: :code-py:`bool`
+   :default: :code-py:`True`
+
+   Write all reading durations to a JSON file ``sphinx_reading_durations.json``
+   in the build directory. File paths and durations (in seconds) are saved as
+   keys and values, respectively.
+
+   .. versionadded:: 8.3
--- sphinx/ext/duration.py
+++ sphinx/ext/duration.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import json
 import time
 from datetime import datetime, timedelta
 from itertools import islice
@@ -27,26 +28,12 @@ if TYPE_CHECKING:
 
 
 DEFAULT_OPTIONS = {
-    'n_durations' : 5,
+    'print_total': True,
+    'print_slowest' : True,
+    'n_slowest' : 5,
     'write_durations' : True,
 }
 
-DURATION_REF = 'sphinx_reading_times'
-
-ORPHAN_FORMAT = """
-:orphan:
-
-.. _{}:
-
-"""
-
-HEADER_FORMAT = (
-        ORPHAN_FORMAT
-        + """
-Computation times
-=================
-"""
-)
 logger = logging.getLogger(__name__)
 
 
@@ -105,29 +92,37 @@ def on_build_finished(app: Sphinx, error: Exception) -> None:
     reading_durations = domain.reading_durations
     if not reading_durations:
         return
-    sorted_durations = sorted(
-        reading_durations.items(), key=itemgetter(1), reverse=True
-    )
 
-    logger.info('')
-    logger.info(
-        __('====================== slowest reading durations =======================')
-    )
     # Get default options and update with user-specified values
     options = DEFAULT_OPTIONS.copy()
     user_options = getattr(app.config, 'duration_options', DEFAULT_OPTIONS)
     options.update(user_options)
 
-    # Log the durations
-    n_durations = options['n_durations']
-    durations_out = {}
-    n_durations = len(sorted_durations) if n_durations == -1 else n_durations
-    for docname, d in islice(sorted_durations, n_durations):
-        durations_out[docname] = d
-        logger.info(f'{_sec_to_readable(d)} {docname}')  # NoQA: G004
+    if options['print_total']:
+        logger.info('')
+        logger.info(__('====================== total reading duration =========================='))
+
+        n_files = len(reading_durations)
+        s = '' if n_files == 1 else 's'
+
+        total = sum(reading_durations.values())
+        logger.info(f'Total time reading {n_files} file{s}:\n')
+        logger.info(_format_seconds(total, multiline=True))
+
+    if options['print_slowest']:
+        logger.info('')
+        logger.info(__('====================== slowest reading durations ======================='))
+        sorted_durations = sorted(reading_durations.items(), key=itemgetter(1), reverse=True)
+        n_slowest = options['n_slowest']
+        n_slowest = len(sorted_durations) if n_slowest == -1 else n_slowest
+        for docname, d in islice(sorted_durations, n_slowest):
+            logger.info(f'{_format_seconds(d)} {docname}')
 
     if options['write_durations']:
-        write_durations(app.builder.srcdir, reading_durations)
+        # Write to JSON
+        out_file = Path(app.builder.outdir) / 'sphinx_reading_durations.json'
+        with out_file.open('w', encoding='utf-8') as fid:
+            json.dump(reading_durations, fid, indent=4)  # indent makes it more readable
 
 
 def setup(app: Sphinx) -> dict[str, bool | str]:
@@ -144,42 +139,17 @@ def setup(app: Sphinx) -> dict[str, bool | str]:
     }
 
 
-def _sec_to_readable(t):
-    """Convert a number of seconds to a more readable representation."""
-    # This will only work for < 1 day execution time
-    # And we reserve 2 digits for minutes because presumably
-    # there aren't many > 99 minute scripts, but occasionally some
-    # > 9 minute ones
-    t = datetime(1, 1, 1) + timedelta(seconds=t)
-    t = f'{t.hour * 60 + t.minute:02d}:{t.second:02d}.{int(round(t.microsecond / 1000.0)):03d}'
-    return t
-
-
-def write_durations(target_dir, reading_durations):
-    """Write durations to `sphinx_reading_times.rst`."""
-    total_time = sum(reading_durations.values())
-    out_file = Path(target_dir) / f'{DURATION_REF}.rst'
-    if out_file.is_file() and total_time == 0:  # a re-run
-        return
-    with out_file.open('w', encoding='utf-8') as fid:
-        fid.write(HEADER_FORMAT.format(DURATION_REF))
-        fid.write(
-            f'**{_sec_to_readable(total_time)}** total reading time for '
-            f'{len(reading_durations)} file{"s" if len(reading_durations) != 1 else ""}:\n\n'
+def _format_seconds(t, multiline=False):
+    """Convert seconds to a formatted string."""
+    dt = datetime(1, 1, 1) + timedelta(seconds=t)
+    minutes = dt.hour * 60 + dt.minute
+    seconds = dt.second
+    milliseconds = int(round(dt.microsecond / 1000.0))
+    if multiline:
+        return (
+            f'minutes:      {minutes:>3}\n'
+            f'seconds:      {seconds:>3}\n'
+            f'milliseconds: {milliseconds:>3}'
         )
-        fid.write("""\
-.. list-table::
-   :header-rows: 1
-   :class: table table-striped sg-datatable
-
-   * - File
-     - Time (mm:ss.SSS)
-""")
-        # Need at least one entry or Sphinx complains
-        for file, duration in reading_durations.items() or [['N/A', 'N/A']]:
-            fid.write(
-                f"""\
-   * - {file}
-     - {_sec_to_readable(duration)}
-"""
-            )
+    else:
+        return f'{minutes:02d}:{seconds:02d}.{milliseconds:03d}'
