2b8f6dab31d5fef15f034b84be8e7e946b9fe961
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2025-06-16 23:09:54 +0100

    Make parsing more explicit in ``sphinx.testing.restructuredtext`` (#13671)

--- sphinx/testing/restructuredtext.py
+++ sphinx/testing/restructuredtext.py
@@ -1,42 +1,77 @@
 from __future__ import annotations
 
+import warnings
 from typing import TYPE_CHECKING
 
-from docutils.core import publish_doctree
+from docutils import nodes
+from docutils.frontend import OptionParser
 
-from sphinx.io import SphinxStandaloneReader
+from sphinx.io import SphinxBaseReader
 from sphinx.parsers import RSTParser
-from sphinx.util.docutils import sphinx_domains
+from sphinx.transforms import SphinxTransformer
+from sphinx.util.docutils import LoggingReporter, sphinx_domains
 
 if TYPE_CHECKING:
-    from docutils import nodes
+    from docutils.frontend import Values
 
     from sphinx.application import Sphinx
 
 
 def parse(app: Sphinx, text: str, docname: str = 'index') -> nodes.document:
-    """Parse a string as reStructuredText with Sphinx application."""
+    """Parse a string as reStructuredText with Sphinx."""
+    config = app.config
     env = app.env
+    registry = app.registry
+    srcdir = app.srcdir
+
+    source_path = str(srcdir / f'{docname}.rst')
+
+    # Get settings
+    settings_overrides = {
+        'gettext_compact': True,
+        'input_encoding': 'utf-8',
+        'output_encoding': 'unicode',
+        'traceback': True,
+    }
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
+        option_parser = OptionParser(
+            components=(RSTParser, SphinxBaseReader), defaults=settings_overrides
+        )
+    settings: Values = option_parser.get_default_values()  # type: ignore[assignment]
+    settings._source = source_path
+    settings.env = env
+
+    # Create parser
+    parser = RSTParser()
+    parser._config = config
+    parser._env = env
+
+    # Create root document node
+    reporter = LoggingReporter(
+        source_path,
+        settings.report_level,
+        settings.halt_level,
+        settings.debug,
+        settings.error_encoding_error_handler,
+    )
+    document = nodes.document(settings, reporter, source=source_path)
+    document.note_source(source_path, -1)
+
+    # substitute transformer
+    document.transformer = transformer = SphinxTransformer(document)
+    transformer.add_transforms(SphinxBaseReader().get_transforms())
+    transformer.add_transforms(registry.get_transforms())
+    transformer.add_transforms(parser.get_transforms())
+
+    env.current_document.docname = docname
     try:
-        app.env.current_document.docname = docname
-        reader = SphinxStandaloneReader()
-        reader._setup_transforms(app.registry.get_transforms())
-        parser = RSTParser()
-        parser._config = app.config
-        parser._env = app.env
         with sphinx_domains(env):
-            return publish_doctree(
-                text,
-                str(app.srcdir / f'{docname}.rst'),
-                reader=reader,
-                parser=parser,
-                settings_overrides={
-                    'env': env,
-                    'gettext_compact': True,
-                    'input_encoding': 'utf-8',
-                    'output_encoding': 'unicode',
-                    'traceback': True,
-                },
-            )
+            parser.parse(text, document)
+            document.current_source = document.current_line = None
+
+            transformer.apply_transforms()
     finally:
         env.current_document.docname = ''
+
+    return document
