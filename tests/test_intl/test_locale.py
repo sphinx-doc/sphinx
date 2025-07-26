2a70ce1582e0a7404aeaa5a2636a9f02e00adf61
Adam Dangoor <adamdangoor@gmail.com>
2025-07-26 21:37:45 +0100

    Remove mypy overrides for tests/test_intl/test_locale.py

--- tests/test_intl/test_locale.py
+++ tests/test_intl/test_locale.py
@@ -9,17 +9,19 @@ import pytest
 from sphinx import locale
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Callable, Generator
     from pathlib import Path
+
+    from sphinx.testing.util import SphinxTestApp
 
 
 @pytest.fixture(autouse=True)
-def _cleanup_translations():
+def _cleanup_translations() -> Generator[None, None, None]:
     yield
     locale.translators.clear()
 
 
-def test_init(rootdir):
+def test_init(rootdir: Path) -> None:
     # not initialized yet
     _ = locale.get_translation('myext')
     assert _('Hello world') == 'Hello world'
@@ -48,7 +50,7 @@ def test_init(rootdir):
     assert _('Hello reST') == 'Hello reST'
 
 
-def test_init_with_unknown_language(rootdir):
+def test_init_with_unknown_language(rootdir: Path) -> None:
     locale.init([rootdir / 'test-locale' / 'locale1'], 'unknown', 'myext')
     _ = locale.get_translation('myext')
     assert _('Hello world') == 'Hello world'
@@ -57,7 +59,7 @@ def test_init_with_unknown_language(rootdir):
 
 
 @pytest.mark.sphinx('html', testroot='root')
-def test_add_message_catalog(app, rootdir):
+def test_add_message_catalog(app: SphinxTestApp, rootdir: Path) -> None:
     app.config.language = 'en'
     app.add_message_catalog('myext', rootdir / 'test-locale' / 'locale1')
     _ = locale.get_translation('myext')
@@ -73,7 +75,9 @@ def _empty_language_translation(rootdir: Path) -> Callable[[str], str]:
     return locale.get_translation(catalog)
 
 
-def test_init_environment_language(rootdir, monkeypatch):
+def test_init_environment_language(
+    rootdir: Path, monkeypatch: pytest.MonkeyPatch
+) -> None:
     with monkeypatch.context() as m:
         m.setenv('LANGUAGE', 'en_US:en')
         _ = _empty_language_translation(rootdir)
