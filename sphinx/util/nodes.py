35a1eea4cb6b33b485bb12edb82cc89c63a051ff
Adam Dangoor <adamdangoor@gmail.com>
2025-09-24 07:03:26 +0100

    Fix a type hint

--- sphinx/util/nodes.py
+++ sphinx/util/nodes.py
@@ -395,7 +395,7 @@ def traverse_translatable_index(
 
 def nested_parse_with_titles(
     state: RSTState, content: StringList, node: Node, content_offset: int = 0
-) -> str:
+) -> int:
     """Version of state.nested_parse() that allows titles and does not require
     titles to have the same decoration as the calling document.
 
