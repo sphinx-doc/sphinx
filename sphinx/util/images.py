5f58f8b9dad37b35a14c6dee5712ab25ffe49bf5
Shengyu Zhang <i@silverrainz.me>
2025-10-29 16:51:09 +0800

    Remove unused base64 import

--- sphinx/util/images.py
+++ sphinx/util/images.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import base64
 from pathlib import Path
 from typing import TYPE_CHECKING, NamedTuple, overload
 from urllib.request import DataHandler, build_opener
