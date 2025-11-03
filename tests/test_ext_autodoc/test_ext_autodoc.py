8c86bad0ef0f6ab99a7fc4529ea5f6dd62721b94
Adam Turner <9087854+AA-Turner@users.noreply.github.com>
2025-11-03 00:30:53 +0000

    Remove ``env`` from autodoc interfaces (#14018)

--- tests/test_ext_autodoc/test_ext_autodoc.py
+++ tests/test_ext_autodoc/test_ext_autodoc.py
@@ -7,6 +7,7 @@ source file translated by test_build.
 from __future__ import annotations
 
 import itertools
+import logging
 import sys
 from typing import TYPE_CHECKING
 from warnings import catch_warnings
@@ -15,6 +16,7 @@ import pytest
 from docutils.statemachine import StringList
 
 from sphinx import addnodes
+from sphinx.environment import _CurrentDocument
 from sphinx.ext.autodoc._directive_options import (
     _AutoDocumenterOptions,
     inherited_members_option,
@@ -53,14 +55,20 @@ if TYPE_CHECKING:
 processed_signatures = []
 
 
-@pytest.mark.sphinx('html', testroot='root')
-def test_parse_name(app):
-    env = app.env
-    current_document = env.current_document
+def test_parse_name(caplog: pytest.LogCaptureFixture) -> None:
+    # work around sphinx.util.logging.setup()
+    logger = logging.getLogger('sphinx')
+    logger.handlers[:] = [caplog.handler]
+
+    current_document = _CurrentDocument()
+    ref_context: dict[str, Any] = {}
 
     def parse(objtype, name):
         parsed = _parse_name(
-            name=name, objtype=objtype, current_document=current_document, env=env
+            name=name,
+            objtype=objtype,
+            current_document=current_document,
+            ref_context=ref_context,
         )
         if parsed is None:
             return None
@@ -74,7 +82,7 @@ def test_parse_name(app):
     assert parsed == ('test.test_ext_autodoc', [], None, None)
     parsed = parse('module', 'test(arg)')
     assert parsed is None
-    assert 'signature arguments' in app.warning.getvalue()
+    assert 'signature arguments given for automodule' in caplog.messages[0]
 
     # for functions/classes
     parsed = parse('function', 'test_ext_autodoc.raises')
@@ -86,18 +94,18 @@ def test_parse_name(app):
     assert parsed == ('test_ext_autodoc', ['raises'], None, None)
     current_document.autodoc_module = ''
 
-    env.ref_context['py:module'] = 'test_ext_autodoc'
+    ref_context['py:module'] = 'test_ext_autodoc'
     parsed = parse('function', 'raises')
     assert parsed == ('test_ext_autodoc', ['raises'], None, None)
     parsed = parse('class', 'Base')
     assert parsed == ('test_ext_autodoc', ['Base'], None, None)
 
     # for members
-    env.ref_context['py:module'] = 'sphinx.testing.util'
+    ref_context['py:module'] = 'sphinx.testing.util'
     parsed = parse('method', 'SphinxTestApp.cleanup')
     assert parsed == ('sphinx.testing.util', ['SphinxTestApp', 'cleanup'], None, None)
-    env.ref_context['py:module'] = 'sphinx.testing.util'
-    env.ref_context['py:class'] = 'Foo'
+    ref_context['py:module'] = 'sphinx.testing.util'
+    ref_context['py:class'] = 'Foo'
     current_document.autodoc_class = 'SphinxTestApp'
     parsed = parse('method', 'cleanup')
     assert parsed == ('sphinx.testing.util', ['SphinxTestApp', 'cleanup'], None, None)
@@ -555,7 +563,9 @@ def _assert_getter_works(app, get_attr, options, objtype, name, *attrs):
     env = app.env
     config = app.config
     current_document = env.current_document
-    events = env.events
+    events = app.events
+    ref_context = env.ref_context
+    reread_always = env.reread_always
     getattr_spy.clear()
 
     props = _load_object_by_name(
@@ -565,22 +575,24 @@ def _assert_getter_works(app, get_attr, options, objtype, name, *attrs):
         type_aliases=config.autodoc_type_aliases,
         current_document=current_document,
         config=config,
-        env=env,
         events=events,
         get_attr=get_attr,
         options=options,
+        ref_context=ref_context,
+        reread_always=reread_always,
     )
     if props is not None:
         _generate_directives(
             config=config,
             current_document=current_document,
-            env=env,
             events=events,
             get_attr=get_attr,
             indent='',
             options=options,
             props=props,
             record_dependencies=set(),
+            ref_context=ref_context,
+            reread_always=reread_always,
             result=StringList(),
         )
 
