9153bf49941f02d2a7abd7830cfeaee1c24b0f42
daniel.eades <daniel.eades@seebyte.com>
2025-12-03 20:19:16 +0000

    fixup

--- tests/test_config/test_config.py
+++ tests/test_config/test_config.py
@@ -161,26 +161,26 @@ def test_config_pickle_circular_reference_in_list():
     check_is_serializable(actual.b, circular=True)
 
     assert actual.a[0] == 1
-    assert actual.extract_node(a, 1, 0) == 2
-    assert actual.extract_node(a, 1, 1, 0) == 1
-    assert actual.extract_node(a, 1, 1, 1, 0) == 2
+    assert actual.a[1][0] == 2
+    assert actual.a[1][1][0] == 1
+    assert actual.a[1][1][1][0] == 2
 
     assert actual.b[0] == 2
-    assert actual.extract_node(b, 1, 0) == 1
-    assert actual.extract_node(b, 1, 1, 0) == 2
-    assert actual.extract_node(b, 1, 1, 1, 0) == 1
+    assert actual.b[1][0] == 1
+    assert actual.b[1][1][0] == 2
+    assert actual.b[1][1][1][0] == 1
 
     assert len(actual.a) == 2
     assert len(actual.a[1]) == 2
-    assert len(actual.extract_node(a, 1, 1)) == 2
-    assert len(actual.extract_node(a, 1, 1, 1)) == 2
-    assert len(actual.extract_node(a, 1, 1, 1, 1)) == 2
+    assert len(actual.a[1][1]) == 2
+    assert len(actual.a[1][1][1]) == 2
+    assert len(actual.a[1][1][1][1]) == 2
 
     assert len(actual.b) == 2
     assert len(actual.b[1]) == 2
-    assert len(actual.extract_node(b, 1, 1)) == 2
-    assert len(actual.extract_node(b, 1, 1, 1)) == 2
-    assert len(actual.extract_node(b, 1, 1, 1, 1)) == 2
+    assert len(actual.b[1][1]) == 2
+    assert len(actual.b[1][1][1]) == 2
+    assert len(actual.b[1][1][1][1]) == 2
 
     def check(
         u: list[list[object] | int],
@@ -213,10 +213,10 @@ def test_config_pickle_circular_reference_in_list():
     #   check(actual.a[0], a[0]) -> ++counter[dict]
     #       ++counter[int] (a[0] is an int)
     #   check(actual.a[1], a[1]) -> ++counter[dict]
-    #       check(actual.extract_node(a, 1, 0), extract_node(a, 1, 0)) -> ++counter[dict]
-    #           ++counter[int] (extract_node(a, 1, 0) is an int)
-    #       check(actual.extract_node(a, 1, 1), extract_node(a, 1, 1)) -> ++counter[dict]
-    #           recursive guard since extract_node(a, 1, 1) == a
+    #       check(actual.a[1][0], a[1][0]) -> ++counter[dict]
+    #           ++counter[int] (a[1][0] is an int)
+    #       check(actual.a[1][1], a[1][1]) -> ++counter[dict]
+    #           recursive guard since a[1][1] == a
     assert counter[type(a[0])] == 2
     assert counter[type(a[1])] == 4
 
