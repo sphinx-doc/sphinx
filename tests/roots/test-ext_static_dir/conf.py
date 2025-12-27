b5154627c45b1b6cbe3a0b2e8790df9d2b53041e
Jared Dillard <jared.dillard@gmail.com>
2025-12-26 18:35:29 -0800

    Fix module name collision

--- tests/roots/test-ext_static_dir/conf.py
+++ tests/roots/test-ext_static_dir/conf.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
 
-# Add the test root to path so we can import myext
+# Add the test root to path so we can import the extension
 sys.path.insert(0, str(Path(__file__).parent))
 
 project = 'Test Extension Static Dir'
-extensions = ['myext']
+extensions = ['staticdirext']
