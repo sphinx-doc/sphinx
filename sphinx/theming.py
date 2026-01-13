1bfb878be78ba91ace29826f587350897fcda5d7
DESKTOP-LF576SH\micha <m.hughes@shaw.ca>
2026-01-13 15:41:13 -0700

    Merge inherited stylesheets in HTML themes (refs: #9093)
    Previously, if a theme defined a stylesheet, it would completely
    overwrite the stylesheet list of any parent themes. This caused
    inherited themes (like 'agogo' or 'traditional') to lose the
    foundation 'basic.css' from the 'basic' theme.
    
    This change:
    1.  Modifies Theme initialization to append inherited stylesheets
        rather than replacing them.
    2.  Ensures stylesheets are deduplicated while preserving order.
    3.  Updates existing tests that relied on the previous replacement
        behavior to expect the full inheritance chain.

--- sphinx/theming.py
+++ sphinx/theming.py
@@ -81,7 +81,9 @@ class Theme:
         for config in reversed(configs.values()):
             options |= config.options
             if config.stylesheets is not None:
-                self.stylesheets = config.stylesheets
+                for stylesheet in config.stylesheets:
+                    if stylesheet not in self.stylesheets:
+                        self.stylesheets += (stylesheet,)
             if config.sidebar_templates is not None:
                 self.sidebar_templates = config.sidebar_templates
             if config.pygments_style_default is not None:
