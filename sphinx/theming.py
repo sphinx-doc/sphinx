e8bd450a71530e2625d034bad61bf70643517e7e
DESKTOP-LF576SH\micha <m.hughes@shaw.ca>
2026-01-14 12:50:28 -0700

    fix: resolve TranslationProxies and avoid TypeErrors during membership checks

--- sphinx/theming.py
+++ sphinx/theming.py
@@ -82,8 +82,11 @@ class Theme:
             options |= config.options
             if config.stylesheets is not None:
                 for stylesheet in config.stylesheets:
-                    if stylesheet not in self.stylesheets:
-                        self.stylesheets += (stylesheet,)
+                    # Force conversion to string to resolve TranslationProxies
+                    # and avoid TypeErrors during membership checks.
+                    stylesheet_str = str(stylesheet)
+                    if stylesheet_str not in self.stylesheets:
+                        self.stylesheets += (stylesheet_str,)
             if config.sidebar_templates is not None:
                 self.sidebar_templates = config.sidebar_templates
             if config.pygments_style_default is not None:
