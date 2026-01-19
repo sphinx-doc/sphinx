9a1595efda3bb6d2f17a3dd81f94b057f9ca4de3
James Addison <jay@jp-hosting.net>
2026-01-19 12:10:17 +0000

    Unwind a `str.format` refactoring in preparation for an alternative approach
    Partially reverts commit d1bc46137a303acbe4b7ffe8090f6ca63f27535a.

--- sphinx/writers/texinfo.py
+++ sphinx/writers/texinfo.py
@@ -252,11 +252,9 @@ class TexinfoTranslator(SphinxTranslator):
                 '(%s)' % elements['filename'],
                 self.escape_arg(self.settings.texinfo_dir_description),
             )
-            elements['direntry'] = (
-                '@dircategory {}\n@direntry\n{}@end direntry\n'.format(
-                    self.escape_id(self.settings.texinfo_dir_category),
-                    entry,
-                )
+            elements['direntry'] = '@dircategory %s\n@direntry\n%s@end direntry\n' % (
+                self.escape_id(self.settings.texinfo_dir_category),
+                entry,
             )
         elements['copying'] = COPYING % elements
         # allow the user to override them all
