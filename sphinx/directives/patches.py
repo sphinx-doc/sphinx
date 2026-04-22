441ae8fc8aa472faa47b8510cbf16b7f3e9d522f
armorbreak001 <contact@agentvote.cc>
2026-04-22 22:13:41 +0800

    Add emphasize-lines support to the code directive
    The .. code:: directive did not accept the :emphasize-lines:
    option even though .. code-block:: does. This caused an unknown

--- sphinx/directives/patches.py
+++ sphinx/directives/patches.py
@@ -13,6 +13,7 @@ from docutils.parsers.rst.directives.misc import Meta
 from sphinx.directives import optional_int
 from sphinx.locale import __
 from sphinx.util import logging
+from sphinx.util._lines import parse_line_num_spec
 from sphinx.util.docutils import SphinxDirective, _normalize_options
 from sphinx.util.nodes import set_source_info
 from sphinx.util.osutil import SEP, relpath
@@ -94,6 +95,7 @@ class Code(SphinxDirective):
         'force': directives.flag,
         'name': directives.unchanged,
         'number-lines': optional_int,
+        'emphasize-lines': directives.unchanged_required,
     }
     has_content = True
 
@@ -102,6 +104,24 @@ class Code(SphinxDirective):
 
         self.options = _normalize_options(self.options)
         code = '\n'.join(self.content)
+
+        linespec = self.options.get('emphasize-lines')
+        if linespec:
+            try:
+                nlines = len(self.content)
+                hl_lines = parse_line_num_spec(linespec, nlines)
+                if any(i >= nlines for i in hl_lines):
+                    logger.warning(
+                        __('line number spec is out of range(1-%d): %r'),
+                        nlines,
+                        self.options['emphasize-lines'],
+                    )
+                hl_lines = [x + 1 for x in hl_lines if x < nlines]
+            except ValueError as err:
+                return [self.state.document.reporter.warning(err, line=self.lineno)]
+        else:
+            hl_lines = None
+
         node = nodes.literal_block(
             code,
             code,
@@ -109,6 +129,9 @@ class Code(SphinxDirective):
             force='force' in self.options,
             highlight_args={},
         )
+        if hl_lines is not None:
+            node['highlight_args']['hl_lines'] = hl_lines
+
         self.add_name(node)
         set_source_info(self, node)
 
