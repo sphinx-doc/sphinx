018c5efc8c70b250d78794607801bc2e01f3c911
Jared Dillard <jared.dillard+github@gmail.com>
2026-07-31 14:25:38 -0700

    Fix Napoleon handling of exception-raising attributes

--- sphinx/ext/napoleon/__init__.py
+++ sphinx/ext/napoleon/__init__.py
@@ -464,12 +464,12 @@ def _skip_member(
         False if it should be included in the docs.
 
     """
-    has_doc = getattr(obj, '__doc__', False)
+    has_doc = inspect.safe_getattr(obj, '__doc__', False)
     is_member = what in {'class', 'exception', 'module'}
     if name != '__weakref__' and has_doc and is_member:
         cls_is_owner = False
         if what in {'class', 'exception'}:
-            qualname = getattr(obj, '__qualname__', '')
+            qualname = inspect.safe_getattr(obj, '__qualname__', '')
             cls_path, _, _ = qualname.rpartition('.')
             if cls_path:
                 try:
